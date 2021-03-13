0c5cc2c4a4c8e2151acbccabe4b6d86ad5d9063f
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-12 23:28:02 -0300

    refactor: remove unsed

--- src/hooks/forceUpdate.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-
-export function useForceUpdate() {
-  const [value, setValue] = useState(0);
-  console.log('update');
-  return () => setValue(value => ++value);
-}
