425d39076ef8aad1b2c0be7ae7fe7611e2497c64
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-16 20:49:04 -0300

    feat: create component Alert

---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Container } from './style';
+
+type AlertProps = {
+  message: string;
+  isActive: boolean;
+  onClickConfirmButton: () => void;
+  onClickCancellButton: () => void;
+};
+
+export const Alert = ({
+  message,
+  onClickCancellButton,
+  onClickConfirmButton,
+  isActive,
+}: AlertProps): JSX.Element => {
+  return (
+    <Container isActive={isActive}>
+      <main>
+        <h4>{message}</h4>
+        <hr/>
+        <footer>
+          <button
+            className="btn btn-default btn-sm sbold uppercase"
+            onClick={onClickConfirmButton}
+          >
+            Ok
+          </button>
+          <button
+            className="btn dark btn-sm sbold uppercase"
+            onClick={onClickCancellButton}
+          >
+            Cancelar
+          </button>
+        </footer>
+      </main>
+    </Container>
+  );
+};
