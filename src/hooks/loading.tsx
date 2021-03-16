7c3a88befe9211101e80988459343d86f4e4a308
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-16 20:12:34 -0300

    feat: create hooks for loading

---
+++ src/hooks/loading.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+
+interface LoaddingContext {
+  loading: boolean;
+  activeLoading: () => void;
+  disableLoading: () => void;
+}
+
+const LoadingContext = createContext<LoaddingContext>({} as LoaddingContext);
+
+const LoadingProvider: React.FC = ({ children }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const activeLoading = useCallback(() => {
+    setLoading(true);
+  }, [loading]);
+
+  const disableLoading = useCallback(() => {
+    setLoading(false);
+  }, [loading]);
+
+  return (
+    <LoadingContext.Provider value={{ activeLoading, disableLoading, loading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+function useLoading(): LoaddingContext {
+  const context = useContext(LoadingContext);
+
+  if (!context) {
+    throw new Error('useLoading must be used witin a LoadingProvider');
+  }
+
+  return context;
+}
+
+export { LoadingProvider, useLoading };
