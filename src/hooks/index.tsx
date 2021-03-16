40f65d877f9fcb758bb1e7f0d518149a0f34b196
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-16 20:13:55 -0300

    refactor: use Loading Provider

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import { AuthProvider } from './auth';
 import { ToastProvider } from './toast';
+import { LoadingProvider } from './loading';
 
 const AppProvider: React.FC = ({ children }) => (
   <AuthProvider>
-    <ToastProvider>{children}</ToastProvider>
+    <ToastProvider>
+      <LoadingProvider>{children}</LoadingProvider>
+    </ToastProvider>
   </AuthProvider>
 );
 
