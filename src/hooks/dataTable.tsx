58d002ca0e0b6f8b36c6657f0c41029877419845
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-17 16:57:29 -0300

    feat: create hook for update datatable

---
+++ src/hooks/dataTable.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+
+interface DataTableContext {
+  isUpdated: boolean;
+  updateDataTable: () => void;
+}
+
+const DataTableContext = createContext<DataTableContext>(
+  {} as DataTableContext,
+);
+
+const DataTableProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isUpdated, setIsUpdate] = useState<boolean>(false);
+
+  const updateDataTable = useCallback(() => {
+    setIsUpdate(!isUpdated);
+  }, [isUpdated]);
+
+  return (
+    <DataTableContext.Provider value={{ updateDataTable, isUpdated }}>
+      {children}
+    </DataTableContext.Provider>
+  );
+};
+
+function useUpdateDataTable(): DataTableContext {
+  const context = useContext(DataTableContext);
+
+  if (!context) {
+    throw new Error('Updated Provider must be used witin a DataTableProvider');
+  }
+
+  return context;
+}
+
+export { DataTableProvider, useUpdateDataTable };
--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import { AuthProvider } from './auth';
 import { ToastProvider } from './toast';
 import { LoadingProvider } from './loading';
+import { DataTableProvider } from './dataTable';
 
 const AppProvider: React.FC = ({ children }) => (
   <AuthProvider>
     <ToastProvider>
-      <LoadingProvider>{children}</LoadingProvider>
+      <LoadingProvider>
+        <DataTableProvider>{children}</DataTableProvider>
+      </LoadingProvider>
     </ToastProvider>
   </AuthProvider>
 );
