0d5cad8aea0c0a791b34d80196895e93803d6a2e
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-18 00:56:04 -0300

    refactor: change route main file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,65 +1,19 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-
 import Route from './Route';
-import SignIn from '../pages/Signin';
-import SignUp from '../pages/Signup';
-import Dashboard from '../pages/Dashboard';
-
-import ProductCategoriesList from '../pages/Warehouse/ProductCategories/List';
-import ProductCategoriesUpdate from '../pages/Warehouse/ProductCategories/Update';
-import ProductCategoriesCreate from '../pages/Warehouse/ProductCategories/Create';
-import ProductCategoriesView from '../pages/Warehouse/ProductCategories/View';
+import { SignIn, SignUp, Dashboard } from './users/users-router';
+import { ProductCategoriesRouter } from './products/categories/categories-router';
+import { ProductAttributesRouter } from './products/attributes/ProductAtributesRouter';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
     <Route path="/dashboard" component={Dashboard} isPrivate />
-
-    <Route
-      path="/productCategories"
-      exact
-      component={ProductCategoriesList}
-      isPrivate
-    />
-    <Route
-      path="/productCategories/create"
-      component={ProductCategoriesCreate}
-      isPrivate
-    />
-    <Route
-      path="/productCategories/view/:id"
-      component={ProductCategoriesView}
-      isPrivate
-    />
-    <Route
-      path="/productCategories/update/:id"
-      component={ProductCategoriesUpdate}
-      isPrivate
-    />
-
-    <Route
-      path="/productAttributes"
-      exact
-      component={ProductCategoriesList}
-      isPrivate
-    />
-    <Route
-      path="/productAttributes/create"
-      component={ProductCategoriesUpdate}
-      isPrivate
-    />
-    <Route
-      path="/productAttributes/view/:id"
-      component={ProductCategoriesUpdate}
-      isPrivate
-    />
-    <Route
-      path="/productAttributes/update/:id"
-      component={ProductCategoriesUpdate}
-      isPrivate
-    />
+    <React.Fragment>
+      <ProductAttributesRouter />
+      <ProductCategoriesRouter />
+    </React.Fragment>
   </Switch>
 );
 export default Routes;
