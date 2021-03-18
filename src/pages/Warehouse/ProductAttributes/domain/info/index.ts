43c49270ff329834c83bea36548df5e95de1317c
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-18 00:53:37 -0300

    feat: create domain layer for use infos

---
+++ src/pages/Warehouse/ProductAttributes/domain/info/index.ts
@@ -0,0 +1,59 @@
+type TypeActionNameWithTo = {
+  name: string;
+  to: string;
+  icon: string;
+};
+
+type PortletTitleCrudType = {
+  create: TypeActionNameWithTo;
+  read: TypeActionNameWithTo;
+  listOne: TypeActionNameWithTo;
+  update: TypeActionNameWithTo;
+  delete: TypeActionNameWithTo;
+};
+
+const nameEntity = 'ProductAttribute';
+const nameSource = 'ProductAttributes';
+const namePageTitle = 'Atributos dos produtos';
+
+const nameActionPageMain: TypeActionNameWithTo = {
+  name: 'Atributos',
+  to: '/productAttributes',
+  icon: '',
+};
+
+const nameActions: PortletTitleCrudType = {
+  create: {
+    name: 'Adicionar',
+    to: '/productAttributes/create/',
+    icon: 'fa fa-plus',
+  },
+  read: {
+    name: 'Visualizar',
+    to: '/productAttributes/',
+    icon: 'fa fa-list',
+  },
+  listOne: {
+    name: 'Visualizar',
+    to: '/productAttributes/',
+    icon: 'fa fa-list',
+  },
+  update: {
+    name: 'Atualizar',
+    to: '/productAttributes/update/',
+    icon: 'fa fa-edit',
+  },
+  delete: {
+    name: 'Remover',
+    to: '#!',
+    icon: 'fa fa-remove',
+  },
+};
+
+export {
+  nameEntity,
+  nameSource,
+  namePageTitle,
+  nameActionPageMain,
+  nameActions,
+};
