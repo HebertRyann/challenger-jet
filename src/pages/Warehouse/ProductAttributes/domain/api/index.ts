873e00910879a5ff1c282262beba88f2ccb25093
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-18 00:53:17 -0300

    feat: create domain layer for use api

---
+++ src/pages/Warehouse/ProductAttributes/domain/api/index.ts
@@ -0,0 +1,6 @@
+export const apiCreate = (): string => '/productAttributes/';
+export const apiList = (id?: string): string =>
+  !id ? '/productAttributes/' : `/productAttributes/view/${id}`;
+export const apiUpdate = (id: string): string =>
+  `/productAttributes/update/${id}`;
+export const apiDelete = (id: string): string => `/productAttributes/${id}`;
