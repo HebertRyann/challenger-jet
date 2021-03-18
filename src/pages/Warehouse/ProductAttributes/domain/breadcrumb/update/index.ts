dbc87a8859d0847407c66d9798291f22b1713620
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-18 15:59:49 -0300

    refactor: use TypeBreadcrumb

--- src/pages/Warehouse/ProductAttributes/domain/breadcrumb/update/index.ts
+++ src/pages/Warehouse/ProductAttributes/domain/breadcrumb/update/index.ts
@@ -1,6 +1,7 @@
 import { nameActionPageMain, nameActions } from '../../info';
+import { TypeBreadcrumb } from '../';
 
-export const breadcrumbUpdate: Array<any> = [
+export const breadcrumbUpdate: TypeBreadcrumb[] = [
   {
     name: 'Início',
     to: '/',
