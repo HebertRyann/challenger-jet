b5baeb9b98ac69ddddd0beb2c692f32ca25ad0fe
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-20 10:44:25 -0300

    test: fake data

---
+++ src/pages/Warehouse/product/ProductRegister/Create/fakeData.ts
@@ -0,0 +1,250 @@
+type Faker = {
+  name: string;
+  children: { name: string }[];
+};
+
+export const fakeCatetory: Faker[] = [
+  {
+    name: 'Category 1',
+    children: [
+      {
+        name: 'Subcategory 1',
+      },
+      {
+        name: 'Subcategory 2',
+      },
+      {
+        name: 'Subcategory 3',
+      },
+      {
+        name: 'Subcategory 4',
+      },
+      {
+        name: 'Subcategory 5',
+      },
+    ],
+  },
+  {
+    name: 'Category 2',
+    children: [
+      {
+        name: 'Subcategory 1',
+      },
+      {
+        name: 'Subcategory 2',
+      },
+      {
+        name: 'Subcategory 3',
+      },
+      {
+        name: 'Subcategory 4',
+      },
+      {
+        name: 'Subcategory 5',
+      },
+    ],
+  },
+  {
+    name: 'Category 3',
+    children: [
+      {
+        name: 'Subcategory 1',
+      },
+      {
+        name: 'Subcategory 2',
+      },
+      {
+        name: 'Subcategory 3',
+      },
+      {
+        name: 'Subcategory 4',
+      },
+      {
+        name: 'Subcategory 5',
+      },
+    ],
+  },
+  {
+    name: 'Category 4',
+    children: [
+      {
+        name: 'Subcategory 1',
+      },
+      {
+        name: 'Subcategory 2',
+      },
+      {
+        name: 'Subcategory 3',
+      },
+      {
+        name: 'Subcategory 4',
+      },
+      {
+        name: 'Subcategory 5',
+      },
+    ],
+  },
+  {
+    name: 'Category 5',
+    children: [
+      {
+        name: 'Subcategory 1',
+      },
+      {
+        name: 'Subcategory 2',
+      },
+      {
+        name: 'Subcategory 3',
+      },
+      {
+        name: 'Subcategory 4',
+      },
+      {
+        name: 'Subcategory 5',
+      },
+    ],
+  },
+  {
+    name: 'Category 6',
+    children: [
+      {
+        name: 'Subcategory 1',
+      },
+      {
+        name: 'Subcategory 2',
+      },
+      {
+        name: 'Subcategory 3',
+      },
+      {
+        name: 'Subcategory 4',
+      },
+      {
+        name: 'Subcategory 5',
+      },
+    ],
+  },
+];
+
+export const fakeFinancy = [
+  {
+    name: 'Finance 1',
+    children: [
+      {
+        name: 'SubFinance 1',
+      },
+      {
+        name: 'SubFinance 2',
+      },
+      {
+        name: 'SubFinance 3',
+      },
+      {
+        name: 'SubFinance 4',
+      },
+      {
+        name: 'SubFinance 5',
+      },
+    ],
+  },
+  {
+    name: 'Finance 2',
+    children: [
+      {
+        name: 'SubFinance 1',
+      },
+      {
+        name: 'SubFinance 2',
+      },
+      {
+        name: 'SubFinance 3',
+      },
+      {
+        name: 'SubFinance 4',
+      },
+      {
+        name: 'SubFinance 5',
+      },
+    ],
+  },
+  {
+    name: 'Finance 3',
+    children: [
+      {
+        name: 'SubFinance 1',
+      },
+      {
+        name: 'SubFinance 2',
+      },
+      {
+        name: 'SubFinance 3',
+      },
+      {
+        name: 'SubFinance 4',
+      },
+      {
+        name: 'SubFinance 5',
+      },
+    ],
+  },
+  {
+    name: 'Finance 4',
+    children: [
+      {
+        name: 'SubFinance 1',
+      },
+      {
+        name: 'SubFinance 2',
+      },
+      {
+        name: 'SubFinance 3',
+      },
+      {
+        name: 'SubFinance 4',
+      },
+      {
+        name: 'SubFinance 5',
+      },
+    ],
+  },
+  {
+    name: 'Finance 5',
+    children: [
+      {
+        name: 'SubFinance 1',
+      },
+      {
+        name: 'SubFinance 2',
+      },
+      {
+        name: 'SubFinance 3',
+      },
+      {
+        name: 'SubFinance 4',
+      },
+      {
+        name: 'SubFinance 5',
+      },
+    ],
+  },
+  {
+    name: 'Finance 6',
+    children: [
+      {
+        name: 'SubFinance 1',
+      },
+      {
+        name: 'SubFinance 2',
+      },
+      {
+        name: 'SubFinance 3',
+      },
+      {
+        name: 'SubFinance 4',
+      },
+      {
+        name: 'SubFinance 5',
+      },
+    ],
+  },
+];
