6adb4514cd65b3f384461b39f2f7abf28dd82881
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-25 14:26:09 -0300

    refactor: return children in array for pattern

--- src/pages/Warehouse/product/ProductRegister/Create/api/load.ts
+++ src/pages/Warehouse/product/ProductRegister/Create/api/load.ts
@@ -1,17 +1,19 @@
 import { stringify } from 'uuid';
 import api from '../../../../../../services/api';
 
-type CategoryData = {
+export type ResponseEntiryWithIdName = {
   id: string;
   name: string;
   parent_id: string | null;
 };
 
-export const loadCategoryData = async (): Promise<CategoryData[]> => {
+export const loadCategoryData = async (): Promise<
+  ResponseEntiryWithIdName[]
+> => {
   const { data, status } = await api.get('/productCategories/list/');
 
   if (status === 200) {
-    const resultData: CategoryData[] = [];
+    const resultData: ResponseEntiryWithIdName[] = [];
     data.map((result: any) =>
       resultData.push({
         id: result.id,
@@ -24,11 +26,30 @@ export const loadCategoryData = async (): Promise<CategoryData[]> => {
   return [];
 };
 
-export const loadCategoryFinance = async (): Promise<CategoryData[]> => {
+export const loadCategoryFinance = async (): Promise<
+  ResponseEntiryWithIdName[]
+> => {
   const { data, status } = await api.get('/financialCategories');
 
   if (status === 200) {
-    const resultData: CategoryData[] = [];
+    const resultData: ResponseEntiryWithIdName[] = [];
+    data.map((result: any) =>
+      resultData.push({
+        id: result.id,
+        name: result.name,
+        parent_id: result.parent_id,
+      }),
+    );
+    return resultData;
+  }
+  return [];
+};
+
+export const loadAtributes = async (): Promise<ResponseEntiryWithIdName[]> => {
+  const { data, status } = await api.get('/productAttributes');
+
+  if (status === 200) {
+    const resultData: ResponseEntiryWithIdName[] = [];
     data.map((result: any) =>
       resultData.push({
         id: result.id,
