52774b52547043889c999b832e3e67b459c8a6f9
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-26 19:58:04 -0300

    feat: create component for render over view tab

---
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/DataOverview/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Container } from './styled';
+
+export const labelDataOverview = 'Dados';
+export const nameDataOverview = '@@tabs-overview';
+
+export const DataOverview = (): JSX.Element => {
+  return <h1>{labelDataOverview}</h1>;
+};
