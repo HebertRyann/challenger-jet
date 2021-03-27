fa6e35c81eb18faf04b9dd6d4983acb5476930e6
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-26 22:05:14 -0300

    refactor: add class disable

--- src/pages/Warehouse/product/ProductRegister/Create/components/footer/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/components/footer/index.tsx
@@ -5,7 +5,7 @@ import { Container } from './styles';
 export const FooterCreateProduct = (): JSX.Element => {
   return (
     <Container>
-      <button className="btn dark btn-sm sbold uppercase">
+      <button className="btn disabled dark btn-sm sbold uppercase">
         <span
           className="fa fa-arrow-left"
           aria-hidden="true"
