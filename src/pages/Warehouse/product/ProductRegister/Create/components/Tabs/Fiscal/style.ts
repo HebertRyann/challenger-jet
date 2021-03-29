c4e4899d84664a993f1f44730a73bb4c8148307e
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-29 17:55:42 -0300

    feat: styles for Fiscal

---
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Fiscal/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  .name-fisco {
+    margin: 25px 0;
+  }
+  hr {
+    margin: 15px 0;
+  }
+  .form-content {
+    > input {
+      margin-bottom: 15px;
+    }
+  }
+`;
