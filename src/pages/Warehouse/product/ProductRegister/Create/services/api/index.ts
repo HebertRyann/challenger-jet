778ab5b7da1d881feec08b29af1d62b74d41388a
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-30 08:22:00 -0300

    feat: create ResponseEntiryWithIdNameWithChildren

--- src/pages/Warehouse/product/ProductRegister/Create/services/api/index.ts
+++ src/pages/Warehouse/product/ProductRegister/Create/services/api/index.ts
@@ -1,18 +1,26 @@
 import api from '../../../../../../../services/api';
 
-export type ResponseEntiryWithIdName = {
+export type ResponseEntityWithIdName = {
   id: string;
   name: string;
   parent_id: string | null;
 };
 
+export type ResponseEntiryWithIdNameWithChildren = {
+  id: string;
+  name: string;
+  parent_id: string | null;
+  childrenList: ResponseEntiryWithIdNameWithChildren[];
+  isChecked?: boolean;
+};
+
 export const loadCategoryData = async (): Promise<
-  ResponseEntiryWithIdName[]
+  ResponseEntityWithIdName[]
 > => {
   const { data, status } = await api.get('/productCategories/list/');
 
   if (status === 200) {
-    const resultData: ResponseEntiryWithIdName[] = [];
+    const resultData: ResponseEntityWithIdName[] = [];
     data.map((result: any) =>
       resultData.push({
         id: result.id,
@@ -26,12 +34,12 @@ export const loadCategoryData = async (): Promise<
 };
 
 export const loadCategoryFinance = async (): Promise<
-  ResponseEntiryWithIdName[]
+  ResponseEntityWithIdName[]
 > => {
   const { data, status } = await api.get('/financialCategories');
 
   if (status === 200) {
-    const resultData: ResponseEntiryWithIdName[] = [];
+    const resultData: ResponseEntityWithIdName[] = [];
     data.map((result: any) =>
       resultData.push({
         id: result.id,
@@ -44,16 +52,19 @@ export const loadCategoryFinance = async (): Promise<
   return [];
 };
 
-export const loadAtributes = async (): Promise<ResponseEntiryWithIdName[]> => {
+export const loadAtributes = async (): Promise<
+  ResponseEntiryWithIdNameWithChildren[]
+> => {
   const { data, status } = await api.get('/productAttributes');
 
   if (status === 200) {
-    const resultData: ResponseEntiryWithIdName[] = [];
+    const resultData: ResponseEntiryWithIdNameWithChildren[] = [];
     data.map((result: any) =>
       resultData.push({
         id: result.id,
         name: result.name,
         parent_id: result.parent_id,
+        childrenList: [],
       }),
     );
     return resultData;
