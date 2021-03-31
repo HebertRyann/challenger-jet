e1c1c10a1f70ad7608f43756ae174657b5c6420d
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-30 23:25:52 -0300

    feat: create temp component new select

---
+++ src/components/NewSelect/index.tsx
@@ -0,0 +1,31 @@
+import React, { SelectHTMLAttributes } from 'react';
+import { Container } from './styles';
+
+export type TypeErrorSelect = {
+  isError: boolean;
+  descriptionError: string;
+};
+
+export interface TypeNewSelectProps
+  extends SelectHTMLAttributes<HTMLSelectElement> {
+  error?: TypeErrorSelect;
+  children: JSX.Element[];
+}
+
+export const NewSelect = ({
+  children,
+  error,
+  ...props
+}: TypeNewSelectProps): JSX.Element => {
+  return (
+    <Container className="selectdiv" isError={error?.isError}>
+      <select className="form-control" {...props}>
+        <option disabled selected>
+          selecione
+        </option>
+        {children}
+      </select>
+      {error && <label>{error.descriptionError}</label>}
+    </Container>
+  );
+};
