0f47ff81b5d761847a7a103ec14e4c209f373916
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-31 22:45:36 -0300

    fix: remove error 429 many request, add params for components

--- src/pages/Warehouse/product/ProductRegister/Create/components/Content/tabs.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Content/tabs.tsx
@@ -5,6 +5,7 @@ import {
   DataOverview,
   labelDataOverview,
   nameDataOverview,
+  TypeEntityWithIdAndName,
 } from '../Tabs/DataOverview';
 
 import { Details, nameDetails, labelDetails } from '../Tabs/Details';
@@ -32,52 +33,105 @@ import {
 } from '../Tabs/PriceComposition';
 
 import { TabsProvider } from '../../../../../../../hooks/tabs';
+import {
+  loadCategoryFinance,
+  loadUnitMensured,
+  loadCategoryData,
+  ResponseEntityOnlyIdAndName,
+  ResponseEntiryWithIdNameWithChildren,
+  loadAtributes,
+} from '../../services/api';
 
-export const makeTabs = (): TypeContentTabs[] => [
-  {
-    label: labelDataOverview,
-    name: nameDataOverview,
-    isEnable: true,
-    Component: <DataOverview />,
-  },
-  {
-    label: labelDetails,
-    name: nameDetails,
-    isEnable: true,
-    Component: <Details />,
-  },
-  {
-    label: labelStock,
-    name: nameStock,
-    isEnable: true,
-    Component: <Stock />,
-  },
-  {
-    label: labelHasVariation,
-    name: nameHasVariation,
-    isEnable: false,
-    Component: <HasVariation />,
-  },
-  {
-    label: labelPriceComposition,
-    name: namePriceComposition,
-    isEnable: false,
-    Component: <PriceComposition />,
-  },
-  {
-    label: labelFiscal,
-    name: nameFiscal,
-    isEnable: false,
-    Component: (
-      <TabsProvider>
-        <Fiscal />
-      </TabsProvider>
-    ),
-  },
-  {
-    label: labelHasComposition,
-    name: nameHasComposition,
-    isEnable: false,
-    Component: <HasComposition />,
-  },
-];
+export const makeTabs = async (): Promise<TypeContentTabs[]> => {
+  const loadCategoryFinances = async (): Promise<TypeEntityWithIdAndName[]> => {
+    const result = await loadCategoryFinance();
+    return result;
+  };
+  const categoryFinances = await loadCategoryFinances();
+
+  const loadUnitMensureds = async (): Promise<
+    ResponseEntityOnlyIdAndName[]
+  > => {
+    const result = await loadUnitMensured();
+    return result;
+  };
+
+  const unitMensureds = await loadUnitMensureds();
+
+  const loadCategoryPorducts = async (): Promise<TypeEntityWithIdAndName[]> => {
+    const result = await loadCategoryData();
+    return result;
+  };
+  const categoryPorducts = await loadCategoryPorducts();
+
+  const loadAtributesList = async () => {
+    const resultData = await loadAtributes();
+    const resultList: ResponseEntiryWithIdNameWithChildren[] = resultData.filter(
+      ({ parent_id }) => parent_id === null,
+    );
+    resultList.map(({ id }, index) => {
+      resultList[index].childrenList = resultData.filter(
+        ({ parent_id }) => parent_id === id,
+      );
+    });
+    return resultList;
+  };
+
+  const atributes = await loadAtributesList();
+
+  return [
+    {
+      label: labelDataOverview,
+      name: nameDataOverview,
+      isEnable: true,
+      Component: (
+        <DataOverview
+          categoryFinances={categoryFinances}
+          categoryProducts={categoryPorducts}
+        />
+      ),
+    },
+    {
+      label: labelDetails,
+      name: nameDetails,
+      isEnable: true,
+      Component: <Details />,
+    },
+    {
+      label: labelStock,
+      name: nameStock,
+      isEnable: true,
+      Component: <Stock unitMensureds={unitMensureds} />,
+    },
+    {
+      label: labelHasVariation,
+      name: nameHasVariation,
+      isEnable: false,
+      Component: (
+        <HasVariation atributes={atributes} unitMensureds={unitMensureds} />
+      ),
+    },
+    {
+      label: labelPriceComposition,
+      name: namePriceComposition,
+      isEnable: false,
+      Component: <PriceComposition />,
+    },
+    {
+      label: labelFiscal,
+      name: nameFiscal,
+      isEnable: false,
+      Component: (
+        <TabsProvider>
+          <Fiscal />
+        </TabsProvider>
+      ),
+    },
+    {
+      label: labelHasComposition,
+      name: nameHasComposition,
+      isEnable: false,
+      Component: <HasComposition />,
+    },
+  ];
+};
