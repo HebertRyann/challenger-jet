166f1276328c4022be09c42c39b7f10c5e0afad8
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-01 17:08:25 -0300

    feat: create TextArea component

--- src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Details/style.ts
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Details/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   textarea {
@@ -12,5 +12,21 @@ export const Container = styled.div`
     ::-webkit-input-placeholder {
       text-align: right;
     }
+    text-align: end;
   }
+`;
+
+export const TextArea = styled.textarea<{ isError: boolean }>`
+  height: 90px;
+  ${({ isError }) =>
+    isError &&
+    css`
+      border: #d43f3a solid 1px !important;
+      background-color: #ebcccc !important;
+      color: #d43f3a;
+      > option {
+        background-color: #fff;
+        color: #666;
+      }
+    `}
 `;
