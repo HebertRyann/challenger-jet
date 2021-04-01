d0d94fcf543c73c29cd31873bb80a8c300baf626
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-31 22:51:39 -0300

    fix: receive unitMensureds and remove use effect

--- src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Stock/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Stock/index.tsx
@@ -1,54 +1,21 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import {
-  loadAtributes,
-  loadUnitMensured,
-  ResponseEntiryWithIdNameWithChildren,
-} from '../../../services/api';
+import React, { useEffect, useState } from 'react';
 import { Table } from './component/Table';
 import { FooterCreateProduct } from '../../footer';
 import { SaveFooter } from '../../footer/saveFooter';
 
-export const Stock = (): JSX.Element => {
-  const [atributesList, setAtributesList] = useState<
-    ResponseEntiryWithIdNameWithChildren[]
-  >([]);
+type TypeHasVariationProps = {
+  unitMensureds: {
+    id: string;
+    name: string;
+  }[];
+};
 
-  const [unitMensured, setUnitMensured] = useState<
-    {
-      id: string;
-      name: string;
-    }[]
-  >([]);
-
-  useEffect(() => {
-    async function load() {
-      const unitMensured = await loadUnitMensured();
-      const resultData = await loadAtributes();
-      const resultList: ResponseEntiryWithIdNameWithChildren[] = resultData.filter(
-        ({ parent_id }) => parent_id === null,
-      );
-      resultList.map(({ id }, index) => {
-        resultList[index].childrenList = resultData.filter(
-          ({ parent_id }) => parent_id === id,
-        );
-      });
-      setUnitMensured(unitMensured);
-      setAtributesList(resultList);
-    }
-    load();
-  }, []);
-
-  const handlerClickCheckBox = useCallback(
-    (index: number) => {
-      atributesList[index].isChecked = !atributesList[index].isChecked;
-      setAtributesList([...atributesList]);
-    },
-    [atributesList],
-  );
-
+export const Stock = ({
+  unitMensureds,
+}: TypeHasVariationProps): JSX.Element => {
   return (
     <div className="row">
-      <Table />
+      <Table unitMensured={unitMensureds} />
       <div style={{ padding: '0 20px' }}>
         <hr />
         <FooterCreateProduct onClickButtonNext={() => {}} />
