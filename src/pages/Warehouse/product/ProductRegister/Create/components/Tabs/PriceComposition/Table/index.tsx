b85813f67efc034af266da4bf85e21596c9deab7
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-01 18:38:08 -0300

    feat: initial validation

--- src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/PriceComposition/Table/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/PriceComposition/Table/index.tsx
@@ -1,37 +1,105 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { TooltipComponent } from '../../../../../../../../../components/TooltipComponent';
+import {
+  NewInput,
+  TypeErrorInput,
+} from '../../../../../../../../../components/NewInput';
 
-export const Table = (): JSX.Element => (
-  <>
-    <div className="row">
-      <div className="form-content col-md-3">
-        <TooltipComponent
-          label="Margem de lucro"
-          message="Selecione o tipo do produto"
-        />
-        <input className="form-control" type="text" />
+export const Table = (): JSX.Element => {
+  const [alert, setAlert] = useState(false);
+  const [profit, setProif] = useState('');
+  const [ipi, setIpi] = useState('');
+  const [cost, setCost] = useState('');
+  const [dif, setDif] = useState('');
+
+  const [errorProfit, setErrorProfit] = useState<TypeErrorInput>({
+    isError: false,
+  });
+
+  const [errorIpi, setErrorIpi] = useState<TypeErrorInput>({
+    isError: false,
+  });
+
+  const [errorCost, setErrorCost] = useState<TypeErrorInput>({
+    isError: false,
+  });
+
+  const [errorDif, setErrorDif] = useState<TypeErrorInput>({
+    isError: false,
+  });
+
+  const handlerChangeProfit = useCallback(
+    (value: string) => {
+      setErrorProfit({ isError: false });
+      setProif(value);
+    },
+    [profit],
+  );
+  const handlerChangeIpi = useCallback(
+    (value: string) => {
+      setErrorIpi({ isError: false });
+      setIpi(value);
+    },
+    [ipi],
+  );
+  const handlerChangeCost = useCallback(
+    (value: string) => {
+      setErrorCost({ isError: false });
+      setCost(value);
+    },
+    [cost],
+  );
+  const handlerChangeDif = useCallback(
+    (value: string) => {
+      setErrorDif({ isError: false });
+      setDif(value);
+    },
+    [dif],
+  );
+
+  const handlerClickNextButton = useCallback(() => {}, [
+    errorProfit,
+    errorIpi,
+    errorCost,
+    errorDif,
+  ]);
+
+  const handlerClickAlertConfirm = useCallback(() => {
+    setAlert(false);
+  }, [alert]);
+
+  return (
+    <>
+      <div className="row">
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="Margem de lucro"
+            message="Selecione o tipo do produto"
+          />
+          <input className="form-control" type="text" />
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="IPI (%)"
+            message="Selecione o tipo do produto"
+          />
+          <input className="form-control" type="text" />
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="Custo fixo"
+            message="Selecione o tipo do produto"
+          />
+          <input className="form-control" type="text" />
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="DIF ICMS"
+            message="Selecione o tipo do produto"
+          />
+          <input className="form-control" type="text" />
+        </div>
       </div>
-      <div className="form-content col-md-3">
-        <TooltipComponent
-          label="IPI (%)"
-          message="Selecione o tipo do produto"
-        />
-        <input className="form-control" type="text" />
-      </div>
-      <div className="form-content col-md-3">
-        <TooltipComponent
-          label="Custo fixo"
-          message="Selecione o tipo do produto"
-        />
-        <input className="form-control" type="text" />
-      </div>
-      <div className="form-content col-md-3">
-        <TooltipComponent
-          label="DIF ICMS"
-          message="Selecione o tipo do produto"
-        />
-        <input className="form-control" type="text" />
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
