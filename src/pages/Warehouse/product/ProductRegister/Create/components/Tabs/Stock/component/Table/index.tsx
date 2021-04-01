405cb429bfd268e0393870426bebdd1936d646ce
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-03-31 22:58:28 -0300

    fix: receive unitMensured and remove use effect, remove code unsed

--- src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Stock/component/Table/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Stock/component/Table/index.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from './style';
-import { ResponseEntityOnlyIdAndName } from '../../../../../services/api';
-import { loadUnitMensured } from '../../../../../services/api';
 
 type TypeUnitMensured = {
   id: string;
@@ -13,39 +11,14 @@ type TypeVariation = {
   isEnable: boolean;
 };
 
-export const Table = (): JSX.Element => {
+type TypeTableProps = {
+  unitMensured: TypeUnitMensured[];
+};
+
+export const Table = ({ unitMensured }: TypeTableProps): JSX.Element => {
   const [variations, setVariations] = useState<TypeVariation[]>([
     { isEnable: true, key: Math.random() },
   ]);
-
-  const [unitMensureds, setUnitMensured] = useState<
-    ResponseEntityOnlyIdAndName[]
-  >([]);
-
-  // const handlerAddNewVariation = useCallback(() => {
-  //   variations.push({ isEnable: true, key: Math.random() });
-  //   setVariations([...variations]);
-  // }, [variations]);
-
-  // const handleRemoveVariation = useCallback(
-  //   (keyVariation: TypeVariation) => {
-  //     variations.map(({ key }, index) => {
-  //       if (key === keyVariation.key) {
-  //         variations[index].isEnable = false;
-  //       }
-  //     });
-  //     setVariations([...variations]);
-  //   },
-  //   [variations],
-  // );
-
-  useEffect(() => {
-    async function load() {
-      const result = await loadUnitMensured();
-      setUnitMensured(result);
-    }
-    load();
-  }, []);
 
   return (
     <Container className="table-responsive">
@@ -54,40 +27,25 @@ export const Table = (): JSX.Element => {
           <tr>
             <th>Unidade de medidas</th>
             <th>Estoque atual</th>
-            {/* <th>Ações</th> */}
           </tr>
-          {variations
-            .filter(({ isEnable }) => isEnable)
-            .map(({ key, isEnable }, index) => (
-              <tr>
-                <td>
-                  <select
-                    className="select form-control"
-                    name="Selecione"
-                    id="Selecione"
-                    defaultValue="Selecione"
-                  >
-                    <option className="disabled" disabled selected>
-                      Selecione
-                    </option>
-                    {unitMensureds.map(
-                      ({ id, name }) => (
-                        <option value={`${id}+${name}`}>{name}</option>
-                      ),
-                      [],
-                    )}
-                  </select>
-                </td>
-                <td>
-                  <input className="form-control" type="text" />
-                </td>
-                {/* <td className="actions">
-                  <IconRemove
-                    onClick={() => handleRemoveVariation({ key, isEnable })}
-                  />
-                </td> */}
-              </tr>
-            ))}
+          <tr>
+            <td>
+              <select className="select form-control" name="Selecione">
+                <option className="disabled" disabled selected>
+                  Selecione
+                </option>
+                {unitMensured.map(
+                  ({ id, name }) => (
+                    <option value={`${id}+${name}`}>{name}</option>
+                  ),
+                  [],
+                )}
+              </select>
+            </td>
+            <td>
+              <input className="form-control" type="text" />
+            </td>
+          </tr>
         </tbody>
       </table>
     </Container>
