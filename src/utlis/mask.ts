4a93266819fed9ce2926c5980ef7bf726d03631f
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-04 10:37:01 -0300

    feat: create function for use an mask numerics

---
+++ src/utlis/mask.ts
@@ -0,0 +1,13 @@
+export function numericMask(value: string) {
+  let newvalue = value.replace(/\D/g, '');
+  newvalue = newvalue.replace(/(\d{2})$/, ',$1');
+  newvalue = newvalue.replace(/(\d+)(\d{3},\d{2})$/g, '$1.$2');
+  const qtdLoop = (newvalue.length - 3) / 3;
+  let count = 0;
+  while (qtdLoop > count) {
+    count++;
+    newvalue = newvalue.replace(/(\d+)(\d{3}.*)/, '$1.$2');
+  }
+  newvalue = newvalue.replace(/^(0)(\d)/g, '$2');
+  return newvalue;
+}
