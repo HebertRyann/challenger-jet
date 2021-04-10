493e240bdd10b6ae76e98dbc9e4017b2b5a98326
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-09 23:28:31 -0300

    feat: create service saveProduct

---
+++ src/pages/Warehouse/product/ProductRegister/Create/services/api/saveProduct.ts
@@ -0,0 +1,31 @@
+import {
+  CompositionRequest,
+  PriceCompositionAndFiscal,
+  TypeProductStock,
+  TypeProductDataOverView,
+  ResultOnSaveProdut,
+} from '../../providers/domain.types';
+import api from '../../../../../../../services/api';
+type SaveProductParams = {
+  composition?: CompositionRequest[];
+  price_composition_fiscal?: PriceCompositionAndFiscal;
+  stock: TypeProductStock[];
+  details_overview: TypeProductDataOverView;
+};
+
+export const saveProduct = async (
+  params: SaveProductParams,
+): Promise<ResultOnSaveProdut> => {
+  const { status } = await api.post('/product', { params });
+  if (status !== 201) {
+    return {
+      status: { code: status, message: 'Erro ao cadastrar o produto' },
+    };
+  }
+  return {
+    status: {
+      code: 201,
+      message: 'Produto cadastrado com sucesso',
+    },
+  };
+};
