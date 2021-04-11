9d9ecb2ac1c08b8a6f70decdfc151f0e25dc9d6e
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-11 12:31:23 -0300

    feat: use ipi provider and new select component

--- src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Fiscal/tabs/Pis/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Fiscal/tabs/Pis/index.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
-import { Select } from '../../../../../../../../../../components/Select';
+import { NewSelect } from '../../../../../../../../../../components/NewSelect';
 import { TooltipComponent } from '../../../../../../../../../../components/TooltipComponent';
 import { dataIcms, TypeICMS } from '../Icms/icms';
-export const nameFiscalPis = '@@tabs-fiscal-pis';
-export const labelFiscalPis = 'Pis';
+import { useTabCreate } from '../../../../../providers/tabsProvider';
 
 export const Pis = (): JSX.Element => {
+  const { fiscal } = useTabCreate();
+  const { changePisTaxeIssue } = fiscal.setData;
+  const { pis } = fiscal.getData();
   return (
     <div className="row">
       <div className="form-content col-md-6">
         <TooltipComponent
           label="Situação tributaria"
-          message="Selecione o tipo do produto"
+          message="Situação tributaria do produto"
         />
-        <Select<TypeICMS>
-          data={dataIcms}
-          selectValue={{ id: 0, name: 'Selecione' }}
-        ></Select>
+        <NewSelect
+          onChange={event => {
+            const split = event.target.value.split('+');
+            const id = split[0];
+            const name = split[1];
+            changePisTaxeIssue({ id, name });
+          }}
+          error={pis.taxesIssue.error}
+        >
+          {dataIcms.map(({ id, name }) => (
+            <option value={`${id}+${name}`}>{name}</option>
+          ))}
+        </NewSelect>
       </div>
     </div>
   );
 };
+
+export const nameFiscalPis = '@@tabs-fiscal-pis';
+export const labelFiscalPis = 'Pis';
