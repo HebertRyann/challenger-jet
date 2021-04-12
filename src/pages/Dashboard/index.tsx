5671a4a7c59df1fa77d12a854f3dbf4d30b811c7
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-12 10:52:44 -0300

    refactor: update style dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => (
+  <div className="page-head">
+    <div className="container-fluid">
+      <div className="page-title">
+        <h1>Dashboard</h1>
+      </div>
+    </div>
+  </div>
+);
 
 export default Dashboard;
