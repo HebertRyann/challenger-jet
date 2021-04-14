8e87dcbf0d1a7f77e06860764af40017c4900964
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-14 12:37:59 -0300

    feat: send params tools from Content component

--- src/pages/Warehouse/product/ProductRegister/Create/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/index.tsx
@@ -6,20 +6,17 @@ import { toolsCreate } from '../domain/tools/create';
 import { Content } from './components/Content';
 import { TabsProvider } from '../../../../../hooks/tabs';
 
-const Create = (): JSX.Element => {
-  return (
-    <Container
-      Content={() => (
-        <TabsProvider>
-          <Content />
-        </TabsProvider>
-      )}
-      pageTitle={namePageTitle}
-      portletTitle={nameActions.create.name}
-      breadcrumb={breadcrumbCreate}
-      tools={[toolsCreate]}
-    />
-  );
-};
+const Create = (): JSX.Element => (
+  <Container
+    Content={() => (
+      <TabsProvider>
+        <Content tools={[toolsCreate]} />
+      </TabsProvider>
+    )}
+    pageTitle={namePageTitle}
+    portletTitle={nameActions.create.name}
+    breadcrumb={breadcrumbCreate}
+  />
+);
 
 export default Create;
