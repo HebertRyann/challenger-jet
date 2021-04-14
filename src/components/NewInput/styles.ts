1f43da70cacad39462e30b34894b3f065fb7fda0
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-14 18:18:47 -0300

    feat: create container ContainerSearch

--- src/components/NewInput/styles.ts
+++ src/components/NewInput/styles.ts
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.div<{ isError?: boolean }>`
+  position: relative;
   ${({ isError }) =>
     isError &&
     css`
@@ -17,5 +18,40 @@ export const Container = styled.div<{ isError?: boolean }>`
   > label {
     margin-top: 5px;
     color: #d43f3a !important;
+  }
+`;
+
+export const ContainerSearch = styled.div<{ active: boolean }>`
+  display: none;
+  ${({ active }) =>
+    active &&
+    css`
+      display: block;
+    `}
+  z-index: 999;
+  top: 105%;
+
+  background-color: #fff;
+  border: 1px solid #c2cad8;
+  width: 100%;
+  max-height: 200px;
+  overflow-y: scroll;
+  position: absolute;
+  > ul {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    li {
+      border-top: 1px solid #c2cad8;
+      border-bottom: 1px solid #c2cad8;
+      padding: 6px 12px;
+      cursor: pointer;
+      :hover {
+        background-color: #f6f6f6;
+      }
+    }
+    li:last-child {
+      margin-bottom: 0;
+    }
   }
 `;
