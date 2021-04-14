2242e3a1a97f2a1466a6196e77ded881569f2bdb
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-14 12:35:06 -0300

    feat: create and use function handleClickAction

--- src/pages/Warehouse/product/ProductRegister/Create/components/Header/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Header/index.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { ToolsContainerProps } from '../../../../../../../components/Container';
 
-export const HeaderCreateProduct = (): JSX.Element => {
+type HeaderCreateProductProps = {
+  tools: ToolsContainerProps[];
+};
+
+export const HeaderCreateProduct = ({
+  tools,
+}: HeaderCreateProductProps): JSX.Element => {
+  const history = useHistory();
+
+  const handleClickAction = ({
+    hasParams,
+    icon,
+    name,
+    to,
+    handleOnClick,
+  }: ToolsContainerProps) => {
+    if (!handleOnClick) {
+      history.push(`${to}`, {
+        id: hasParams ? hasParams.id : '',
+        value: hasParams ? hasParams.value : '',
+      });
+    }
+    if (handleOnClick !== undefined) {
+      handleOnClick({ hasParams, icon, name, to, handleOnClick });
+    }
+  };
+
   return (
     <div className="portlet-title">
       <div className="caption">Adicionar</div>
 
       <div className="tools">
-        <div
-          style={{
-            cursor: 'pointer',
-          }}
-          key={Math.random()}
-        >
-          <i style={{ marginRight: '5px' }} className="fa fa-list" />
-          Listar
-        </div>
+        {tools.map(tool => (
+          <div
+            style={{
+              cursor: 'pointer',
+            }}
+            key={Math.random()}
+            onClick={() => {
+              handleClickAction(tool);
+            }}
+          >
+            <i style={{ marginRight: '5px' }} className={tool.icon} />
+            {tool.name}
+          </div>
+        ))}
       </div>
     </div>
   );
