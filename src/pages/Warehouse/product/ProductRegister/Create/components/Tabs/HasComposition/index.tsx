e4adb192872b65f5c47d9d2a8eddb20606959fe1
Eric Silva Mendes <ericsilvaccp@gmail.com>
2021-04-16 02:03:35 -0300

    fix: remove class row

--- src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/HasComposition/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/HasComposition/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Table } from './Table';
 
-export const HasComposition = (): JSX.Element => (
-  <div className="row">
-    <Table />
-  </div>
-);
+export const HasComposition = (): JSX.Element => <Table />;
 
 export const labelHasComposition = 'Composição';
 export const nameHasComposition = '@@tabs-has-composition';
