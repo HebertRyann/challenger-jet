5f66b51643fd39e2624187c82c98e4093aef1184
Eric Silva <ericsilvaccp@gmail.com>
2021-04-19 18:41:13 -0300

    feat: create Content tabs for view tab list

---
+++ src/pages/Warehouse/product/ProductRegister/View/Content/index.tsx
@@ -0,0 +1,72 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  Container,
+  ContentItem,
+  RenderComponent,
+  TabHeaderContainer,
+  TabName,
+  TabPanelContainer,
+} from './styles';
+import { useTabs } from '../../../../../../hooks/tabs';
+import { makeTabs } from './tabs';
+
+export type TypeContentTabs = {
+  name: string;
+  label: string;
+  isEnable: boolean;
+  Component: JSX.Element;
+};
+
+type Link = {
+  link: string;
+  name: string;
+};
+
+export const Content = (): JSX.Element => {
+  const [tabs, setTabs] = useState<TypeContentTabs[]>([]);
+
+  const { loadTabs, addTab, loadCurrentTab, changeCurrentTab } = useTabs();
+
+  useEffect(() => {
+    async function load() {
+      const tabs = await makeTabs();
+      tabs.map(tab => addTab(tab));
+      changeCurrentTab(tabs[0].name);
+      setTabs(loadTabs());
+    }
+    load();
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <ContentItem>
+          <TabHeaderContainer>
+            {tabs.map(
+              ({ label, name, isEnable }, index) =>
+                isEnable && (
+                  <TabName
+                    key={index}
+                    onClick={() => changeCurrentTab(name)}
+                    isActive={name === loadCurrentTab().key}
+                  >
+                    {label}
+                  </TabName>
+                ),
+            )}
+          </TabHeaderContainer>
+          <TabPanelContainer>
+            <hr />
+            <>
+              {tabs.map(({ Component, name }) => (
+                <RenderComponent isActive={name === loadCurrentTab().key}>
+                  {Component}
+                </RenderComponent>
+              ))}
+            </>
+          </TabPanelContainer>
+        </ContentItem>
+      </Container>
+    </>
+  );
+};
