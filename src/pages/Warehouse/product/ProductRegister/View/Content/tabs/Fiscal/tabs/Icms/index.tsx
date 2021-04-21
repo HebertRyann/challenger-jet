62a5e777afb1ce45ec1940e8768dbb8eadfdf227
Eric Silva <ericsilvaccp@gmail.com>
2021-04-21 00:41:23 -0300

    feat: create tabs for use in Fiscal

---
+++ src/pages/Warehouse/product/ProductRegister/View/Content/tabs/Fiscal/tabs/Icms/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Icms = (): JSX.Element => {
+  return (
+    <div className="row">
+      <div className="form-content col-md-6">
+        <div className="form-content col-md-3">
+          <label htmlFor="Peso">NCM</label>
+          <p>4</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const nameFiscalIcms = '@@tabs-fiscal-icms';
+export const labelFiscalIcms = 'ICMS';
