d26647d3052a4eeb2e765eadf67eaf28942609fa
Eric Silva <ericsilvaccp@gmail.com>
2021-04-21 00:39:21 -0300

    refactor: add abas HasVariation, HasComposition, PriceComposition and Fiscal

--- src/pages/Warehouse/product/ProductRegister/View/Content/tabs.tsx
+++ src/pages/Warehouse/product/ProductRegister/View/Content/tabs.tsx
@@ -3,6 +3,22 @@ import { TypeContentTabs } from './index';
 
 import { Details, labelDetails, nameDetails } from './tabs/Details';
 import { Stock, labelStock, nameStock } from './tabs/Stock';
+import { Fiscal, labelFiscal, nameFiscal } from './tabs/Fiscal';
+import {
+  PriceComposition,
+  labelPriceComposition,
+  namePriceComposition,
+} from './tabs/PriceComposition';
+import {
+  HasVariation,
+  labelHasVariation,
+  nameHasVariation,
+} from './tabs/HasVariation';
+import {
+  HasComposition,
+  labelHasComposition,
+  nameHasComposition,
+} from './tabs/HasComposition';
 
 export const makeTabs = async (): Promise<TypeContentTabs[]> => {
   return [
@@ -17,6 +33,30 @@ export const makeTabs = async (): Promise<TypeContentTabs[]> => {
       name: nameStock,
       isEnable: true,
       Component: <Stock />,
+    },
+    {
+      label: labelHasVariation,
+      name: nameHasVariation,
+      isEnable: false,
+      Component: <HasVariation />,
+    },
+    {
+      label: labelHasComposition,
+      name: nameHasComposition,
+      isEnable: false,
+      Component: <HasComposition />,
+    },
+    {
+      label: labelPriceComposition,
+      name: namePriceComposition,
+      isEnable: true,
+      Component: <PriceComposition />,
+    },
+    {
+      label: labelFiscal,
+      name: nameFiscal,
+      isEnable: true,
+      Component: <Fiscal />,
     },
   ];
 };
