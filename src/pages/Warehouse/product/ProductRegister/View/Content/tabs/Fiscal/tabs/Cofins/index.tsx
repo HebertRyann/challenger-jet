3665bb396471451a9f4abdae36694ca9aca5f5c1
Eric Silva <ericsilvaccp@gmail.com>
2021-04-21 00:40:34 -0300

    feat: create Confins for use in Fiscal aba

---
+++ src/pages/Warehouse/product/ProductRegister/View/Content/tabs/Fiscal/tabs/Cofins/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Confins = (): JSX.Element => {
+  return (
+    <div className="row">
+      <div className="form-content col-md-6">
+        <div className="form-content col-md-3">
+          <label htmlFor="Peso">NCM</label>
+          <p>4</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const labelFiscalConfins = 'Confins';
+export const nameFiscalConfins = '@@tabs-fiscal-cofins';
