fc3d2fbeb9aa5cddddf58d33f6e2cc70c396cef1
Eric Silva <ericsilvaccp@gmail.com>
2021-04-21 00:43:45 -0300

    refactor: change type in domain layer for use in response from backend

--- src/pages/Warehouse/product/ProductRegister/View/domain/response/productResponse.ts
+++ src/pages/Warehouse/product/ProductRegister/View/domain/response/productResponse.ts
@@ -1,16 +1,67 @@
+export type Atributes = {
+  id: number;
+  parent_id: number | null;
+  key: string;
+  value: string;
+};
+
+export type CompositonView = {
+  name: string;
+  cost: number;
+  amount: number;
+};
+
+export type FiscalView = {
+  ncm: number;
+  cfop: number;
+  icms: {
+    stateTaxe: string;
+    origem: string;
+  };
+  ipi: {
+    stateTaxe: string;
+    origem: string;
+  };
+  pis: {
+    stateTaxe: string;
+    origem: string;
+  };
+  confins: {
+    stateTaxe: string;
+    origem: string;
+  };
+};
+
+export type PriceCompositionView = {
+  dif: number;
+  ipi: number;
+  fixed_cost: number;
+  margin_profit: number;
+};
+
 export type ProductResponse = {
-  id: 4;
+  id: number;
   name: string;
   composition: string | undefined;
   created_at: string;
   updated_at: string;
   deleted_at: string | undefined;
   details: string;
-  financial_category_id: 12;
-  fiscal: null;
-  price_composition: null;
-  product_category_id: 1;
+  fiscal: string | null;
+  price_composition: string | null;
   type: string;
+  subfinancial_category: {
+    id: number;
+    name: string;
+  };
+  financial_category: {
+    id: number;
+    name: string;
+  };
+  product_category: {
+    id: number;
+    name: string;
+  };
   stocks: [
     {
       unit_mensured_id: number;
