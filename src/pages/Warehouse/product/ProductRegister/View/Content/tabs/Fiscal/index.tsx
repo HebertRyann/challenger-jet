a2b7e34f6035e3f601bfc3dabc7e6b13067d2e7d
Eric Silva <ericsilvaccp@gmail.com>
2021-04-26 10:09:10 -0300

    refactor: enable tab fiscal if type is resale or sale

--- src/pages/Warehouse/product/ProductRegister/View/Content/tabs/Fiscal/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/View/Content/tabs/Fiscal/index.tsx
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { useTabs } from '../../../../../../../../hooks/tabs';
 import {
   formatProductTypeToLowerCase,
+  RE_SALE,
   SALE,
-  SEMI_FINISHED,
 } from '../../../../Create/domain/products';
 import { FiscalView } from '../../../domain/response/productResponse';
 import { useProduct } from '../../../provider/productProvider';
@@ -19,7 +19,7 @@ export const Fiscal = (): JSX.Element => {
   useEffect(() => {
     if (
       getProduct().type?.replace(' ', '-') ===
-        formatProductTypeToLowerCase(SEMI_FINISHED) ||
+        formatProductTypeToLowerCase(RE_SALE) ||
       getProduct().type?.replace(' ', '-') ===
         formatProductTypeToLowerCase(SALE)
     ) {
@@ -44,6 +44,50 @@ export const Fiscal = (): JSX.Element => {
         </div>
       </div>
       <hr />
+      <table className="table table-bordered margin-bottom-0">
+        <thead>
+          <tr>
+            <th colSpan={2} style={{ width: '25%' }} className="title">
+              ICMS
+            </th>
+            <th colSpan={2} style={{ width: '25%' }} className="title">
+              IPI
+            </th>
+            <th colSpan={2} style={{ width: '25%' }} className="title">
+              PIS
+            </th>
+            <th colSpan={2} style={{ width: '25%' }} className="title">
+              COFINS
+            </th>
+          </tr>
+          <tr>
+            <th style={{ width: '12.5%' }} className="title">
+              Situação tributaria
+            </th>
+            <th style={{ width: '12.5%' }} className="title">
+              Origem
+            </th>
+            <th colSpan={2} style={{ width: '25%' }} className="title">
+              Situação tributaria
+            </th>
+            <th colSpan={2} style={{ width: '25%' }} className="title">
+              Situação tributaria
+            </th>
+            <th colSpan={2} style={{ width: '25%' }} className="title">
+              Situação tributaria
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="items">
+            <td>{fiscalList?.icms_tax_situation?.toFixed(2)}</td>
+            <td>{fiscalList?.icms_tax_origem?.toFixed(2)}</td>
+            <td colSpan={2}>{fiscalList?.ipi_tax_situation?.toFixed(2)}</td>
+            <td colSpan={2}>{fiscalList?.pis_tax_situation?.toFixed(2)}</td>
+            <td colSpan={2}>{fiscalList?.cofins_tax_situation?.toFixed(2)}</td>
+          </tr>
+        </tbody>
+      </table>
     </Container>
   );
 };
