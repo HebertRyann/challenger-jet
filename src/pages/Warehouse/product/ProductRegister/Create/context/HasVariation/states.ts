5db5372a96fc8fa40f9a22733a1932a8ff804d65
Eric Silva <ericsilvaccp@gmail.com>
2021-04-26 08:09:09 -0300

    feat: create initial states

---
+++ src/pages/Warehouse/product/ProductRegister/Create/context/HasVariation/states.ts
@@ -0,0 +1,22 @@
+import { TypeHasVariationState } from './types';
+
+export const initialStateHasVariation: TypeHasVariationState = {
+  hasVariation: {
+    inputs: [
+      {
+        unitMensured: {
+          value: '',
+        },
+        currentStock: {
+          value: '',
+        },
+        replamenentPoint: { value: '' },
+        price: {
+          cost: { value: '' },
+          sale: { value: '' },
+        },
+        atributes: [{ id: 0, name: '' }],
+      },
+    ],
+  },
+};
