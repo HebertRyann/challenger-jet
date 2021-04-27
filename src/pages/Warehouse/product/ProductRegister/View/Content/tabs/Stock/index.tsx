0bfe46e52c502d2f2d1ea7ec1cb388db6ae0e60d
Eric Silva <ericsilvaccp@gmail.com>
2021-04-27 13:47:51 -0300

    feat: show price sale and cost if type is sale or resale

--- src/pages/Warehouse/product/ProductRegister/View/Content/tabs/Stock/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/View/Content/tabs/Stock/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  formatProductTypeToLowerCase,
+  RE_SALE,
+  SALE,
+} from '../../../../Create/domain/products';
+import { PriceResponse } from '../../../domain/response/productResponse';
 import { useProduct } from '../../../provider/productProvider';
 import { Container } from './style';
 
@@ -7,9 +13,19 @@ type Stock = {};
 export const Stock = (): JSX.Element => {
   const { getProduct } = useProduct();
   const { stocks } = getProduct();
+  let pricesStock = {} as PriceResponse;
+
+  const isSaleOrResaleType = (): boolean => {
+    return (
+      getProduct().type?.replace(' ', '-') ===
+        formatProductTypeToLowerCase(RE_SALE) ||
+      getProduct().type?.replace(' ', '-') ===
+        formatProductTypeToLowerCase(SALE)
+    );
+  };
 
   if (stocks) {
-    const { current_stock, replacement_point, details } = stocks[0];
+    const { current_stock, replacement_point, details, prices } = stocks[0];
 
     let unitMensured: { unit_mensured: { name: string } } = {
       unit_mensured: { name: '' },
@@ -19,20 +35,81 @@ export const Stock = (): JSX.Element => {
       unitMensured = JSON.parse(details);
     }
 
+    if (isSaleOrResaleType() && prices) {
+      pricesStock = JSON.parse(prices?.toLowerCase());
+    }
+
     return (
       <Container className="table table-bordered margin-bottom-0">
         <thead>
           <tr>
-            <th className="title">Unidade de medidas</th>
-            <th className="title">Estoque atual</th>
-            <th className="title">Ponto de reposição</th>
+            <th
+              className="title"
+              style={
+                isSaleOrResaleType()
+                  ? {
+                      position: 'relative',
+                      lineHeight: '50px',
+                    }
+                  : {}
+              }
+              rowSpan={isSaleOrResaleType() ? 2 : 1}
+            >
+              Unidade de medidas
+            </th>
+            <th
+              style={
+                isSaleOrResaleType()
+                  ? {
+                      position: 'relative',
+                      lineHeight: '50px',
+                    }
+                  : {}
+              }
+              rowSpan={isSaleOrResaleType() ? 2 : 1}
+              className="title"
+            >
+              Estoque atual
+            </th>
+            <th
+              style={
+                isSaleOrResaleType()
+                  ? {
+                      position: 'relative',
+                      lineHeight: '50px',
+                    }
+                  : {}
+              }
+              rowSpan={isSaleOrResaleType() ? 2 : 1}
+              className="title"
+            >
+              Ponto de reposição
+            </th>
+            {isSaleOrResaleType() && (
+              <th style={{ textAlign: 'center' }} colSpan={2}>
+                Preço
+              </th>
+            )}
           </tr>
+          {isSaleOrResaleType() && (
+            <tr>
+              <th>Custo</th>
+              <th>Venda</th>
+            </tr>
+          )}
+          {}
         </thead>
         <tbody>
           <tr className="items">
             <td>{unitMensured.unit_mensured.name}</td>
             <td>{current_stock}</td>
             <td>{replacement_point}</td>
+            {isSaleOrResaleType() && (
+              <>
+                <td>{pricesStock.price_cost}</td>
+                <td>{pricesStock.price_sale}</td>
+              </>
+            )}
           </tr>
         </tbody>
       </Container>
