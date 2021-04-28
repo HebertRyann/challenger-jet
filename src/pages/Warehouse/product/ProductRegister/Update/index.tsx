6ea5b898b9b7c0b6e1be3a9a3de1b538d56ba61f
Eric Silva <ericsilvaccp@gmail.com>
2021-04-28 00:19:05 -0300

    refactor: use tab provider

--- src/pages/Warehouse/product/ProductRegister/Update/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/Update/index.tsx
@@ -1,60 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import React from 'react';
 import Container from '../../../../../components/Container';
-import { useLoading } from '../../../../../hooks/loading';
-import api from '../../../../../services/api';
-import { FormCategory } from '../components/Form';
-import { apiList } from '../domain/api';
-import { nameActions, namePageTitle } from '../domain/info';
 import { breadcrumbUpdate } from '../domain/breadcrumb';
-import { toolsUpdate } from '../domain/tools';
-
-const ProductAtributeUpdate = (): JSX.Element => {
-  const { location } = useHistory<{
-    id: string;
-    value: string;
-  }>();
+import { nameActions, namePageTitle } from '../domain/info';
+import { toolsCreate } from '../domain/tools/';
+import { Content } from './components/Content';
+import { TabsProvider } from '../../../../../hooks/tabs';
+import { ProductProvider } from './contextData';
+import { useParams } from 'react-router';
+const Create = (): JSX.Element => {
   const { id } = useParams<{ id: string }>();
-  const { activeLoading, disableLoading } = useLoading();
-  const [categoryItem, setCategoryItem] = useState<{
-    id: string;
-    name: string;
-  }>({ id: '', name: '' });
-
-  useEffect(() => {
-    async function loadData() {
-      activeLoading();
-      const response = await api.get(apiList(id));
-      const { data } = response;
-      setCategoryItem({ id: data.id, name: data.name });
-      disableLoading();
-    }
-
-    loadData();
-  }, []);
   return (
     <Container
+      Content={() => (
+        <TabsProvider>
+          <ProductProvider>
+            <Content id={id} tools={[toolsCreate]} />
+          </ProductProvider>
+        </TabsProvider>
+      )}
       pageTitle={namePageTitle}
       portletTitle={nameActions.update.name}
       breadcrumb={breadcrumbUpdate}
-      tools={[
-        toolsUpdate({
-          id: location.state.id,
-          value: location.state.value,
-        }),
-      ]}
-    >
-      <div className="form-body">
-        <FormCategory
-          valueInput={categoryItem.name}
-          typeForm={{
-            idUpdate: Number(categoryItem.id),
-            inputValue: categoryItem.name,
-          }}
-        />
-      </div>
-    </Container>
+    />
   );
 };
 
-export default ProductAtributeUpdate;
+export default Create;
