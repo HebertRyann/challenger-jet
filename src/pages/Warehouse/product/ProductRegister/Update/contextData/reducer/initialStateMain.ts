8cd6c387e768d4b19a4d876a6fba609e06704d50
Eric Silva <ericsilvaccp@gmail.com>
2021-04-28 10:43:29 -0300

    refactor: change name function for initialStateProduct

--- src/pages/Warehouse/product/ProductRegister/Update/contextData/reducer/initialStateMain.ts
+++
@@ -1,9 +0,0 @@
-import { initialStateDataOvewView } from './dataOverView/states';
-import { MainReducerType } from './index';
-
-export const initialMainState: MainReducerType = {
-  dataOverView: {
-    dataOvewViewState: initialStateDataOvewView,
-    dataOverViewDispatch: () => null,
-  },
-};
