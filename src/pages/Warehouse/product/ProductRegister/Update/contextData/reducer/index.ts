7379907a2f86ff5748a3bc2ecca3726a3a9dca0d
Eric Silva <ericsilvaccp@gmail.com>
2021-04-28 23:44:47 -0300

    refactor: remove files not used

--- src/pages/Warehouse/product/ProductRegister/Update/contextData/reducer/index.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch } from 'react';
-import { TypesDataOvewView } from './dataOverView/types';
-import { TypeDataOverViewActions } from './dataOverView/actions';
-
-export type TypeDataOverViewReducer = {
-  dataOvewViewState: TypesDataOvewView;
-  dataOverViewDispatch: Dispatch<TypeDataOverViewActions>;
-};
