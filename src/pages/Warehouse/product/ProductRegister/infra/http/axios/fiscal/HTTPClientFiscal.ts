3002ab456d9b58e3315225b6d6dc83310f06c277
Eric Silva <ericsilvaccp@gmail.com>
2021-05-11 18:55:00 -0300

    refactor: implements

--- src/pages/Warehouse/product/ProductRegister/infra/http/axios/fiscal/HTTPClientFiscal.ts
+++ src/pages/Warehouse/product/ProductRegister/infra/http/axios/fiscal/HTTPClientFiscal.ts
@@ -1,15 +1,46 @@
+import apiAxios from '../../../../../../../../services/api';
 import { HttpClientLoadNCM } from '../../../../data/protocols/Http/Client/NCM/Load';
 import { HttpClientLoadCFOP } from '../../../../data/protocols/Http/Client/CFOP/Load';
+import { HttpClientLoadTaxSituations } from '../../../../data/protocols/Http/Client/TaxSituations/Load';
 import { LoadAllNCM } from '../../../../domain/useCases/FIscal/NCM/Load';
-import apiAxios from '../../../../../../../../services/api';
 import { LoadAllCFOP } from '../../../../domain/useCases/FIscal/CFOP/Load';
+import { LoadTaxSituations } from '../../../../domain/useCases/FIscal/TaxSituations/Load';
 
-export class HTTPClientFiscal implements HttpClientLoadNCM, HttpClientLoadCFOP {
+export class HTTPClientFiscal
+  implements
+    HttpClientLoadNCM,
+    HttpClientLoadCFOP,
+    HttpClientLoadTaxSituations {
+  constructor(
+    private readonly baseUrlLoadersFiscal: {
+      taxeSituation: string;
+      taxeCfop: string;
+      taxeNcms: string;
+    },
+  ) {}
+
+  async loadAllTaxSituations(): Promise<
+    LoadTaxSituations.LoadTaxSituationsResponse[]
+  > {
+    try {
+      console.log(`consulta API ${this.baseUrlLoadersFiscal.taxeSituation}`);
+      const { data, status } = await apiAxios.get<
+        LoadTaxSituations.LoadTaxSituationsResponse[]
+      >(this.baseUrlLoadersFiscal.taxeSituation);
+      if (status === 200) {
+        return data;
+      }
+      return [];
+    } catch (error) {
+      console.warn(error);
+      return [];
+    }
+  }
   async loadAllCFOP(): Promise<LoadAllCFOP.CFOPResponse[]> {
     try {
-      console.log('consulta API /taxes/cfop');
+      console.log(`consulta API ${this.baseUrlLoadersFiscal.taxeCfop}`);
       const { data, status } = await apiAxios.get<LoadAllCFOP.CFOPResponse[]>(
-        `${process.env.REACT_APP_API_URL}taxes/cfop`,
+        this.baseUrlLoadersFiscal.taxeCfop,
       );
       if (status === 200) {
         return data;
@@ -23,9 +54,9 @@ export class HTTPClientFiscal implements HttpClientLoadNCM, HttpClientLoadCFOP {
 
   async loadAllNCM(): Promise<LoadAllNCM.NCMResponse[]> {
     try {
-      console.log('consulta API /taxes/ncms');
+      console.log(`consulta API ${this.baseUrlLoadersFiscal.taxeNcms}`);
       const { data, status } = await apiAxios.get<LoadAllNCM.NCMResponse[]>(
-        `${process.env.REACT_APP_API_URL}taxes/ncms`,
+        this.baseUrlLoadersFiscal.taxeNcms,
       );
       if (status === 200) {
         return data;
