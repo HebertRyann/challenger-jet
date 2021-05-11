af7df6987f157b467144a17608da0e79f383128b
Eric Silva <ericsilvaccp@gmail.com>
2021-05-11 17:30:14 -0300

    refactor: implements HttpClientLoadCFOP

--- src/pages/Warehouse/product/ProductRegister/infra/http/axios/fiscal/HTTPClientFiscal.ts
+++ src/pages/Warehouse/product/ProductRegister/infra/http/axios/fiscal/HTTPClientFiscal.ts
@@ -1,15 +1,31 @@
 import { HttpClientLoadNCM } from '../../../../data/protocols/Http/Client/NCM/Load';
+import { HttpClientLoadCFOP } from '../../../../data/protocols/Http/Client/CFOP/Load';
 import { LoadAllNCM } from '../../../../domain/useCases/FIscal/NCM/Load';
 import apiAxios from '../../../../../../../../services/api';
+import { LoadAllCFOP } from '../../../../domain/useCases/FIscal/CFOP/Load';
 
-export class HTTPClientFiscal implements HttpClientLoadNCM {
-  constructor(private readonly url: { BASE_LOAD_URL: string }) {}
+export class HTTPClientFiscal implements HttpClientLoadNCM, HttpClientLoadCFOP {
+  async loadAllCFOP(): Promise<LoadAllCFOP.CFOPResponse[]> {
+    try {
+      console.log('consulta API /taxes/cfop');
+      const { data, status } = await apiAxios.get<LoadAllCFOP.CFOPResponse[]>(
+        `${process.env.REACT_APP_API_URL}taxes/cfop`,
+      );
+      if (status === 200) {
+        return data;
+      }
+      return [];
+    } catch (error) {
+      console.warn(error);
+      return [];
+    }
+  }
 
   async loadAllNCM(): Promise<LoadAllNCM.NCMResponse[]> {
     try {
-      console.log('consulta API');
+      console.log('consulta API /taxes/ncms');
       const { data, status } = await apiAxios.get<LoadAllNCM.NCMResponse[]>(
-        this.url.BASE_LOAD_URL,
+        `${process.env.REACT_APP_API_URL}taxes/ncms`,
       );
       if (status === 200) {
         return data;
