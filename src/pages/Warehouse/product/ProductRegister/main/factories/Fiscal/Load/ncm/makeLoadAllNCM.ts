5e0a759ae6d08b2e925c189fa71ba89cf702c348
Eric Silva <ericsilvaccp@gmail.com>
2021-05-11 17:30:58 -0300

    refactor: remove base url

--- src/pages/Warehouse/product/ProductRegister/main/factories/Fiscal/Load/ncm/makeLoadAllNCM.ts
+++ src/pages/Warehouse/product/ProductRegister/main/factories/Fiscal/Load/ncm/makeLoadAllNCM.ts
@@ -3,8 +3,5 @@ import { LoadAllNCMData } from '../../../../../data/useCases/Fiscal/NCM/Load';
 import { HTTPClientFiscal } from '../../../../../infra/http/axios/fiscal/HTTPClientFiscal';
 
 export const makeLoadAllNCM = (): LoadAllNCM => {
-  const urlLoadAllNCM = `${process.env.REACT_APP_API_URL}taxes/ncms`;
-  return new LoadAllNCMData(
-    new HTTPClientFiscal({ BASE_LOAD_URL: urlLoadAllNCM }),
-  );
+  return new LoadAllNCMData(new HTTPClientFiscal());
 };
