9b9219743eaa2e8f704b85ecb4a5731bfc337a85
Eric Silva <ericsilvaccp@gmail.com>
2021-05-11 21:45:45 -0300

    feat: create HttpClientLoadNatureOperations

---
+++ src/pages/Warehouse/product/ProductRegister/data/protocols/Http/Client/NatureOperations/Load/index.ts
@@ -0,0 +1,7 @@
+import { LoadNatureOperations } from '../../../../../../domain/useCases/FIscal/NatureOperations/Load';
+
+export interface HttpClientLoadNatureOperations {
+  loadAllNatureOperations: () => Promise<
+    LoadNatureOperations.NatureOperationsResponse[]
+  >;
+}
