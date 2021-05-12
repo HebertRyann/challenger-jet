6c8436e4e0c87af8c6106695135637213a02a72f
Eric Silva <ericsilvaccp@gmail.com>
2021-05-11 21:46:20 -0300

    refactor: send taxeNatureOperations

--- src/pages/Warehouse/product/ProductRegister/main/factories/Fiscal/Load/httpClientFIscal/makeHttpClientFiscal.ts
+++ src/pages/Warehouse/product/ProductRegister/main/factories/Fiscal/Load/httpClientFIscal/makeHttpClientFiscal.ts
@@ -5,5 +5,6 @@ export const makeHttpClientFiscal = (): HTTPClientFiscal => {
     taxeCfop: `${process.env.REACT_APP_API_URL}taxes/cfop`,
     taxeNcms: `${process.env.REACT_APP_API_URL}taxes/ncms`,
     taxeSituation: `${process.env.REACT_APP_API_URL}taxes/situations`,
+    taxeNatureOperations: `${process.env.REACT_APP_API_URL}taxes/nature_operations`,
   });
 };
