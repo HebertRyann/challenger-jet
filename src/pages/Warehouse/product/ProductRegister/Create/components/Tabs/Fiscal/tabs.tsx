dcd22a1c66367b0a8ea652d4327aeb35c55706ca
Eric Silva <ericsilvaccp@gmail.com>
2021-05-11 21:43:46 -0300

    feat: send params to fiscal

--- src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Fiscal/tabs.tsx
+++ src/pages/Warehouse/product/ProductRegister/Create/components/Tabs/Fiscal/tabs.tsx
@@ -1,29 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TypeContentTabsFiscal } from '.';
 import { labelFiscalIcms, nameFiscalIcms, Icms } from './tabs/Icms';
 import { labelFiscalIpi, nameFiscalIpi, Ipi } from './tabs/Ipi';
 import { labelFiscalPis, nameFiscalPis, Pis } from './tabs/Pis';
 import { labelFiscalConfins, nameFiscalConfins, Confins } from './tabs/Cofins';
 import { makeLoadAllTaxSituations } from '../../../../main/factories/Fiscal/Load/TaxSituations/makeLoadAllCfop';
+import { makeLoadNatureOperations } from '../../../../main/factories/Fiscal/Load/NatureOperations/makeLoadNatureOperations';
 
 export const makeTabsFiscal = (): TypeContentTabsFiscal[] => [
   {
     label: labelFiscalIcms,
     name: nameFiscalIcms,
     isEnable: true,
-    Component: <Icms />,
+    Component: (
+      <Icms
+        taxSituationsLoader={makeLoadAllTaxSituations()}
+        natureOperationsLoader={makeLoadNatureOperations()}
+      />
+    ),
   },
   {
     label: labelFiscalIpi,
     name: nameFiscalIpi,
     isEnable: true,
-    Component: <Ipi />,
+    Component: <Ipi taxSituationsLoader={makeLoadAllTaxSituations()} />,
   },
   {
     label: labelFiscalPis,
     name: nameFiscalPis,
     isEnable: true,
-    Component: <Pis />,
+    Component: <Pis taxSituationsLoader={makeLoadAllTaxSituations()} />,
   },
   {
     label: labelFiscalConfins,
