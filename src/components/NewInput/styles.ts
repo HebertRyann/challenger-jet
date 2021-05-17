823c89f07adc94fc44a95105b2dcc8e10a40a090
Eric Silva <ericsilvaccp@gmail.com>
2021-05-17 18:28:06 -0300

    fix: error border in input

--- src/components/NewInput/styles.ts
+++ src/components/NewInput/styles.ts
@@ -5,14 +5,12 @@ export const Container = styled.div<{ isError?: boolean }>`
   ${({ isError }) =>
     isError &&
     css`
-      > input {
-        border: #d43f3a solid 1px !important;
-        background-color: #ebcccc !important;
-        color: #d43f3a;
-        > option {
-          background-color: #fff;
-          color: #666;
-        }
+      border: #d43f3a solid 1px !important;
+      background-color: #ebcccc !important;
+      color: #d43f3a;
+      > option {
+        background-color: #fff;
+        color: #666;
       }
     `}
   > label {
