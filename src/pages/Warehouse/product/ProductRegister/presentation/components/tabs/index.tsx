815693ea0969748ba89eac3e18eca1fa45d8c31a
Eric Silva <ericsilvaccp@gmail.com>
2021-06-07 13:12:44 -0300

    refactor: create TabComponent

--- src/pages/Warehouse/product/ProductRegister/presentation/components/tabs/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/presentation/components/tabs/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useTabs, TabsProvider } from '../../../../../../../hooks/tabs'
 import { TabsModel } from '../../../domain/models/tabs'
 import {
   Container,
@@ -13,31 +14,52 @@ type TypeContentProps = {
   tabList: TabsModel[]
 }
 
+const TabComponent = ({ tabList }: TypeContentProps): JSX.Element => {
+  const { addTab, loadCurrentTab } = useTabs()
+
+  useEffect(() => {
+    tabList.forEach(({ name, isDefault, label, isEnable, Component }) => {
+      addTab({ default: isDefault, name, label, isEnable, Component })
+    })
+  }, [addTab, tabList])
+
+  return (
+    <TabsProvider>
+      <Container>
+        <ContentItem>
+          <TabHeaderContainer>
+            {tabList.map(
+              ({ label, isEnable, name }, index) =>
+                isEnable && (
+                  <TabName key={index} isActive={loadCurrentTab().key === name}>
+                    {label}
+                  </TabName>
+                )
+            )}
+          </TabHeaderContainer>
+          <TabPanelContainer>
+            <>
+              <hr />
+              {tabList.map(({ Component, name }) => (
+                <RenderComponent
+                  key={Math.random()}
+                  isActive={loadCurrentTab().key === name}
+                >
+                  {Component}
+                </RenderComponent>
+              ))}
+            </>
+          </TabPanelContainer>
+        </ContentItem>
+      </Container>
+    </TabsProvider>
+  )
+}
+
 export const Tab = ({ tabList }: TypeContentProps): JSX.Element => {
   return (
-    <Container>
-      <ContentItem>
-        <TabHeaderContainer>
-          {tabList.map(
-            ({ label, isEnable }, index) =>
-              isEnable && (
-                <TabName key={index} isActive={true}>
-                  {label}
-                </TabName>
-              )
-          )}
-        </TabHeaderContainer>
-        <TabPanelContainer>
-          <>
-            <hr />
-            {tabList.map(({ Component, name }) => (
-              <RenderComponent key={Math.random()} isActive={name === ''}>
-                {Component}
-              </RenderComponent>
-            ))}
-          </>
-        </TabPanelContainer>
-      </ContentItem>
-    </Container>
+    <TabsProvider>
+      <TabComponent tabList={tabList} />
+    </TabsProvider>
   )
 }
