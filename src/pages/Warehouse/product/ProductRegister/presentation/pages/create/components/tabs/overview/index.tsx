b2764ae6f7acd27b23efaf4243c4580693352dec
Eric Silva <ericsilvaccp@gmail.com>
2021-06-07 19:57:05 -0300

    feat: use simple select html

--- src/pages/Warehouse/product/ProductRegister/presentation/pages/create/components/tabs/overview/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/presentation/pages/create/components/tabs/overview/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useFormContext } from 'react-hook-form'
-import { NewSelect } from '../../../../../../../../../../components/NewSelect'
 import { TooltipComponent } from '../../../../../../../../../../components/TooltipComponent'
 import { useFormApplication } from '../../../../../providers/form/FormProvider'
 import { useProduct } from '../../../../../providers/product/ProductProvider'
@@ -12,6 +11,11 @@ export const OverviewTab = (): JSX.Element => {
   const { register } = useFormContext()
   const { errors } = useFormApplication()
 
+  const getError = (error: any): string => {
+    if (error) return 'error'
+    return ''
+  }
+
   return (
     <Container className="row">
       <div className="form-content col-md-3">
@@ -19,36 +23,44 @@ export const OverviewTab = (): JSX.Element => {
           label="Tipo do produto"
           message="Selecione o tipo do produto"
         />
-        <NewSelect
+        <select
+          className={`form-control ${getError(
+            errors?.overview?.selectTypeProduct
+          )}`}
           {...register('overview.selectTypeProduct' as const, {
             required: true
           })}
-          error={{ isError: !!errors?.overview?.selectTypeProduct }}
         >
+          <option className="disable" selected={true} disabled>
+            selecione
+          </option>
           {loadTypeProducts().map(({ key, name, label }) => (
             <option key={key} value={key + '+' + name}>
               {label}
             </option>
           ))}
-        </NewSelect>
+        </select>
       </div>
       <div className="form-content col-md-3">
         <TooltipComponent
           label="Grupo do produto"
           message="selecione o grupo do produto"
         />
-        <NewSelect
+        <select
+          className={`form-control ${getError(errors?.overview?.groupProduct)}`}
           {...register('overview.groupProduct' as const, {
             required: true
           })}
-          error={{ isError: !!errors?.overview?.groupProduct }}
         >
+          <option className="disable" selected={true} disabled>
+            selecione
+          </option>
           {loadGroupProduct().map(({ name, id }) => (
             <option key={id} value={id + '+' + name}>
               {name}
             </option>
           ))}
-        </NewSelect>
+        </select>
       </div>
       <div className="form-content col-md-6">
         <TooltipComponent
