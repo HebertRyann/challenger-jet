dad9983016843cfd0a515209e69307cad5ba4e42
Eric Silva <ericsilvaccp@gmail.com>
2021-06-07 20:03:43 -0300

    refactor: add template component

--- src/pages/Warehouse/product/ProductRegister/presentation/pages/create/components/tabs/overview/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/presentation/pages/create/components/tabs/overview/index.tsx
@@ -17,62 +17,125 @@ export const OverviewTab = (): JSX.Element => {
   }
 
   return (
-    <Container className="row">
-      <div className="form-content col-md-3">
-        <TooltipComponent
-          label="Tipo do produto"
-          message="Selecione o tipo do produto"
-        />
-        <select
-          className={`form-control ${getError(
-            errors?.overview?.selectTypeProduct
-          )}`}
-          {...register('overview.selectTypeProduct' as const, {
-            required: true
-          })}
-        >
-          <option className="disable" selected={true} disabled>
-            selecione
-          </option>
-          {loadTypeProducts().map(({ key, name, label }) => (
-            <option key={key} value={key + '+' + name}>
-              {label}
+    <>
+      <Container className="row">
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="Tipo do produto"
+            message="Selecione o tipo do produto"
+          />
+          <select
+            className={`form-control ${getError(
+              errors?.overview?.selectTypeProduct
+            )}`}
+            {...register('overview.selectTypeProduct' as const, {
+              required: true
+            })}
+          >
+            <option className="disable" selected={true} disabled>
+              selecione
             </option>
-          ))}
-        </select>
-      </div>
-      <div className="form-content col-md-3">
-        <TooltipComponent
-          label="Grupo do produto"
-          message="selecione o grupo do produto"
-        />
-        <select
-          className={`form-control ${getError(errors?.overview?.groupProduct)}`}
-          {...register('overview.groupProduct' as const, {
-            required: true
-          })}
-        >
-          <option className="disable" selected={true} disabled>
-            selecione
-          </option>
-          {loadGroupProduct().map(({ name, id }) => (
-            <option key={id} value={id + '+' + name}>
-              {name}
+            {loadTypeProducts().map(({ key, name, label }) => (
+              <option key={key} value={key + '+' + name}>
+                {label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="Grupo do produto"
+            message="selecione o grupo do produto"
+          />
+          <select
+            className={`form-control ${getError(
+              errors?.overview?.groupProduct
+            )}`}
+            {...register('overview.groupProduct' as const, {
+              required: true
+            })}
+          >
+            <option className="disable" selected={true} disabled>
+              selecione
             </option>
-          ))}
-        </select>
-      </div>
-      <div className="form-content col-md-6">
-        <TooltipComponent
-          label="Nome do produto"
-          message="Selecione o nome do produto"
-        />
-        <InputForm
-          name={'overview.nameProduct'}
-          required
-          error={errors?.overview?.nameProduct}
-        />
-      </div>
-    </Container>
+            {loadGroupProduct().map(({ name, id }) => (
+              <option key={id} value={id + '+' + name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-content col-md-6">
+          <TooltipComponent
+            label="Nome do produto"
+            message="Selecione o nome do produto"
+          />
+          <InputForm
+            name={'overview.nameProduct'}
+            required
+            error={errors?.overview?.nameProduct}
+          />
+        </div>
+      </Container>
+      <Container className="row">
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="Tipo do produto"
+            message="Selecione o tipo do produto"
+          />
+          <select
+            className={`form-control ${getError(
+              errors?.overview?.selectTypeProduct
+            )}`}
+            {...register('overview.selectTypeProduct' as const, {
+              required: true
+            })}
+          >
+            <option className="disable" selected={true} disabled>
+              selecione
+            </option>
+            {loadTypeProducts().map(({ key, name, label }) => (
+              <option key={key} value={key + '+' + name}>
+                {label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="Grupo do produto"
+            message="selecione o grupo do produto"
+          />
+          <select
+            className={`form-control ${getError(
+              errors?.overview?.groupProduct
+            )}`}
+            {...register('overview.groupProduct' as const, {
+              required: true
+            })}
+          >
+            <option className="disable" selected={true} disabled>
+              selecione
+            </option>
+            {loadGroupProduct().map(({ name, id }) => (
+              <option key={id} value={id + '+' + name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-content col-md-6">
+          <TooltipComponent
+            label="Nome do produto"
+            message="Selecione o nome do produto"
+          />
+          <InputForm
+            name={'overview.nameProduct'}
+            required
+            error={errors?.overview?.nameProduct}
+          />
+        </div>
+      </Container>
+    </>
   )
 }
--- src/pages/Warehouse/product/ProductRegister/presentation/pages/create/components/tabs/overview/styles.ts
+++ src/pages/Warehouse/product/ProductRegister/presentation/pages/create/components/tabs/overview/styles.ts
@@ -4,4 +4,8 @@ export const Container = styled.div`
   .disable {
     display: none;
   }
+
+  & + & {
+    margin-top: 20px !important;
+  }
 `
