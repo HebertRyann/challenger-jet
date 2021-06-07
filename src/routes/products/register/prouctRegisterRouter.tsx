a0be62c136a860a703936753f3a9df4abfe94550
Eric Silva <ericsilvaccp@gmail.com>
2021-06-07 07:55:36 -0300

    feat: use page CreateProductPage

--- src/routes/products/register/prouctRegisterRouter.tsx
+++ src/routes/products/register/prouctRegisterRouter.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 // import ProductRegisterUpdate from '../../../pages/Warehouse/product/ProductRegister/Update'
 // import ProductRegisterCreate from '../../../pages/Warehouse/product/ProductRegister/Create'
 // import ProductRegisterView from '../../../pages/Warehouse/product/ProductRegister/View'
+import { CreateProductPage } from '../../../pages/Warehouse/product/ProductRegister/presentation/create/index'
 import Route from '../../Route'
 
 const Render = (): JSX.Element => <h1>Render</h1>
 
 export const ProductRegisterRouter = (): JSX.Element => (
   <>
-    <Route path="/products" exact component={Render} isPrivate />
+    <Route path="/products" exact component={CreateProductPage} isPrivate />
     <Route path="/products/create" component={Render} isPrivate />
     <Route path="/products/view/:id" component={Render} isPrivate />
     <Route path="/products/update/:id" component={Render} isPrivate />
