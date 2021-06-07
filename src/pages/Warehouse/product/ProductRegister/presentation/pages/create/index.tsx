a1df98a324ee33180b5ca2f4aeae2713330237f4
Eric Silva <ericsilvaccp@gmail.com>
2021-06-07 09:10:17 -0300

    feat: use Container component

--- src/pages/Warehouse/product/ProductRegister/presentation/pages/create/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/presentation/pages/create/index.tsx
@@ -3,6 +3,10 @@ import { TabsModel } from '../../../domain/models/tabs'
 import { Tab } from '../../components/tabs/index'
 import { ProductProvider } from '../../providers/product/ProductProvider'
 import { OverviewTab } from './components/tabs/overview'
+import Container from '../../../../../../../components/Container'
+import { breadcrumbCreate } from '../../../domain/data/breadcrumb/create'
+import { namePageTitle, nameActions } from '../../../domain/data/info'
+import { toolsCreate } from '../../../domain/data/tools/create'
 
 const tab: TabsModel = {
   name: '',
@@ -14,7 +18,12 @@ const tab: TabsModel = {
 export const CreateProductPage = (): JSX.Element => {
   return (
     <ProductProvider>
-      <Tab tabList={[tab]} />
+      <Container
+        Content={() => <Tab tabList={[tab]} />}
+        pageTitle={namePageTitle}
+        portletTitle={nameActions.create.name}
+        breadcrumb={breadcrumbCreate}
+      />
     </ProductProvider>
   )
 }
