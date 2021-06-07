aa60b38e90bd4d5051a10bbf3ec3078525faf3a6
Eric Silva <ericsilvaccp@gmail.com>
2021-06-07 13:50:27 -0300

    refactor: change component tabs

--- src/pages/Warehouse/product/ProductRegister/presentation/pages/create/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/presentation/pages/create/index.tsx
@@ -3,23 +3,42 @@ import { TabsModel } from '../../../domain/models/tabs'
 import { Tab } from '../../components/tabs/index'
 import { ProductProvider } from '../../providers/product/ProductProvider'
 import { OverviewTab } from './components/tabs/overview'
+import { DetailsTab } from './components/tabs/details'
 import Container from '../../../../../../../components/Container'
 import { breadcrumbCreate } from '../../../domain/data/breadcrumb/create'
 import { namePageTitle, nameActions } from '../../../domain/data/info'
-import { toolsCreate } from '../../../domain/data/tools/create'
+import { makeLoadProdutctsType } from '../../../main/factories/products/load/makeLoadProdutctsType'
+import { FormProvider } from '../../providers/form/FormProvider'
 
-const tab: TabsModel = {
-  name: '',
-  label: 'teste',
-  isEnable: true,
-  Component: <OverviewTab />
+const tabs: TabsModel[] = [
+  {
+    name: 'overview',
+    label: 'Dados',
+    isEnable: true,
+    isDefault: true,
+    Component: <OverviewTab />
+  },
+  {
+    name: 'details',
+    label: 'Detalhes',
+    isEnable: true,
+    Component: <DetailsTab />
+  }
+]
+
+const RenderContentTabs = (): JSX.Element => {
+  return (
+    <FormProvider>
+      <Tab tabList={tabs} />
+    </FormProvider>
+  )
 }
 
 export const CreateProductPage = (): JSX.Element => {
   return (
-    <ProductProvider>
+    <ProductProvider loadTypeProduct={makeLoadProdutctsType()}>
       <Container
-        Content={() => <Tab tabList={[tab]} />}
+        Content={RenderContentTabs}
         pageTitle={namePageTitle}
         portletTitle={nameActions.create.name}
         breadcrumb={breadcrumbCreate}
