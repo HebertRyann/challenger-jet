16e1ed9cbf6a1a6ffeac192c3ac9edb6dea4a3f0
Eric Silva <ericsilvaccp@gmail.com>
2021-06-07 07:54:49 -0300

    feat: create component Tab

--- src/pages/Warehouse/product/ProductRegister/presentation/tabs/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/presentation/tabs/index.tsx
@@ -1,5 +1,43 @@
 import React from 'react'
+import { TabsModel } from '../../domain/models/tabs'
+import {
+  Container,
+  ContentItem,
+  RenderComponent,
+  TabHeaderContainer,
+  TabName,
+  TabPanelContainer
+} from './styles'
 
-export const Tabs = (): JSX.Element => {
-  return <h1>teste</h1>
+type TypeContentProps = {
+  tabList: TabsModel[]
+}
+
+export const Tab = ({ tabList }: TypeContentProps): JSX.Element => {
+  return (
+    <Container>
+      <ContentItem>
+        <TabHeaderContainer>
+          {tabList.map(
+            ({ label, isEnable }, index) =>
+              isEnable && (
+                <TabName key={index} isActive={true}>
+                  {label}
+                </TabName>
+              )
+          )}
+        </TabHeaderContainer>
+        <TabPanelContainer>
+          <>
+            <hr />
+            {tabList.map(({ Component, name }) => (
+              <RenderComponent key={Math.random()} isActive={name === ''}>
+                {Component}
+              </RenderComponent>
+            ))}
+          </>
+        </TabPanelContainer>
+      </ContentItem>
+    </Container>
+  )
 }
