1bc2cf7b07a6ace95e5ac06d7ed3504652cfe894
Eric Silva <ericsilvaccp@gmail.com>
2021-06-07 08:08:20 -0300

    feat: use components Card in Dashboard

---
+++ src/pages/Dashboard/data.ts
@@ -0,0 +1,50 @@
+import {
+  faMoneyBillWaveAlt,
+  faIndustry,
+  faChartBar,
+  faBalanceScale,
+  faSitemap
+} from '@fortawesome/free-solid-svg-icons'
+
+export const controlsData = [
+  {
+    number: 1,
+    text: 'financeiro',
+    textFooter: 'pendente',
+    valueSubtitleFooter: 0,
+    link: 'financialCategories',
+    icon: faMoneyBillWaveAlt
+  },
+  {
+    number: 1,
+    text: 'atributos',
+    textFooter: 'pendente',
+    valueSubtitleFooter: 0,
+    link: 'financialCategories',
+    icon: faIndustry
+  },
+  {
+    number: 1,
+    text: 'estoque',
+    textFooter: 'pendente',
+    valueSubtitleFooter: 0,
+    link: 'financialCategories',
+    icon: faChartBar
+  },
+  {
+    number: 1,
+    text: 'unidade de medida',
+    textFooter: 'pendente',
+    valueSubtitleFooter: 0,
+    link: 'financialCategories',
+    icon: faBalanceScale
+  },
+  {
+    number: 1,
+    text: 'grupo produto',
+    textFooter: 'pendente',
+    valueSubtitleFooter: 0,
+    link: 'financialCategories',
+    icon: faSitemap
+  }
+]
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import { Card } from '../../components/Card'
+import { ControlPanel } from './style'
+import { controlsData } from './data'
 
-const Dashboard: React.FC = () => (
-  <div className="page-head">
-    <div className="container-fluid">
-      <div className="page-title">
-        <h1>Dashboard</h1>
+const Dashboard = (): JSX.Element => (
+  <>
+    <div className="page-head">
+      <div className="container-fluid">
+        <div className="page-title">
+          <h1>Painel de Controle</h1>
+        </div>
       </div>
     </div>
-  </div>
+    <ControlPanel className="page-content-inner">
+      <h4>Produção</h4>
+      <div className="row">
+        {controlsData.map(
+          ({ icon, link, number, text, textFooter, valueSubtitleFooter }) => (
+            <Card
+              key={Math.random()}
+              number={number}
+              text={text}
+              textFooter={textFooter}
+              isProgress={10}
+              valueSubtitleFooter={valueSubtitleFooter}
+              width="col-lg-2 col-md-3 col-sm-6 col-xs-12"
+              icon={icon}
+              link={link}
+            />
+          )
+        )}
+      </div>
+    </ControlPanel>
+  </>
 )
 
 export default Dashboard
