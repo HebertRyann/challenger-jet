39a4f7b4a4c5ba790ea99e841bb8aa4265726414
Eric Silva <ericsilvaccp@gmail.com>
2021-06-07 07:54:26 -0300

    feat: render Tabs

--- src/pages/Warehouse/product/ProductRegister/presentation/create/index.tsx
+++ src/pages/Warehouse/product/ProductRegister/presentation/create/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import { TabsModel } from '../../domain/models/tabs'
+import { Tab } from '../tabs/index'
+
+const render = (): JSX.Element => <h1>teste</h1>
+
+const tab: TabsModel = {
+  name: '',
+  label: 'teste',
+  isEnable: true,
+  Component: render()
+}
 
 export const CreateProductPage = (): JSX.Element => {
-  return <h1>Create</h1>
+  return <Tab tabList={[tab]} />
 }
