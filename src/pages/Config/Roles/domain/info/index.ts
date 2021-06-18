6ec508e3aa5693fc4d4a579eedfe99f0033ad76a
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-06-18 13:29:19 -0300

    feat: create Roles datatable info

---
+++ src/pages/Config/Roles/domain/info/index.ts
@@ -0,0 +1,53 @@
+type TypeActionNameWithTo = {
+  name: string
+  to: string
+  icon: string
+}
+
+type PortletTitleCrudType = {
+  create: TypeActionNameWithTo
+  read: TypeActionNameWithTo
+  update: TypeActionNameWithTo
+  delete: TypeActionNameWithTo
+}
+
+const nameEntity = 'Role'
+const nameSource = 'roles'
+const namePageTitle = 'Grupos de Usuário'
+
+const nameActionPageMain: TypeActionNameWithTo = {
+  name: 'Grupo',
+  to: '/roles',
+  icon: ''
+}
+
+const nameActions: PortletTitleCrudType = {
+  create: {
+    name: 'Adicionar',
+    to: '/roles/create/',
+    icon: 'fa fa-plus'
+  },
+  read: {
+    name: 'Visualizar',
+    to: '/roles/',
+    icon: 'fa fa-list'
+  },
+  update: {
+    name: 'Atualizar',
+    to: '/roles/update/',
+    icon: 'fa fa-edit'
+  },
+  delete: {
+    name: 'Remover',
+    to: '#!',
+    icon: 'fa fa-remove'
+  }
+}
+
+export {
+  nameEntity,
+  nameSource,
+  namePageTitle,
+  nameActionPageMain,
+  nameActions
+}
