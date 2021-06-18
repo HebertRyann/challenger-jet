db876b7f8020f6e8ec6fb0bd1083beeb67f84b3c
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-06-18 13:28:51 -0300

    feat: create Roles datatable headers

---
+++ src/pages/Config/Roles/domain/headers/index.ts
@@ -0,0 +1,5 @@
+export const headers = [
+  { name: 'Nome', field: 'name', sortable: true },
+  { name: 'Descrição', field: 'description', sortable: true },
+  { name: 'Ações', field: 'actions', sortable: false }
+]
