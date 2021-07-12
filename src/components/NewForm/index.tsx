da69ca5d04a8c8747990dd111ca3c68719bc33e8
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-07-12 11:11:53 -0300

    feat: handle and show Input Error

--- src/components/NewForm/index.tsx
+++ src/components/NewForm/index.tsx
@@ -4,10 +4,23 @@ import React, {
   useEffect,
   ReactElement
 } from 'react'
-import { useForm, UseFormRegister } from 'react-hook-form'
+import {
+  FieldError,
+  RegisterOptions,
+  useForm,
+  UseFormRegister
+} from 'react-hook-form'
+
+import { FiAlertCircle } from 'react-icons/fi'
+import { Contanier, Error } from './styles'
 
 export function Form({ defaultValues, children, onSubmit }: any) {
-  const { handleSubmit, register, reset } = useForm({ defaultValues })
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors }
+  } = useForm({ defaultValues })
 
   useEffect(() => {
     reset(defaultValues)
@@ -18,6 +31,7 @@ export function Form({ defaultValues, children, onSubmit }: any) {
       ...{
         ...child.props,
         register,
+        error: errors[child.props.name],
         key: child.props.name
       }
     })
@@ -48,10 +62,28 @@ export function Form({ defaultValues, children, onSubmit }: any) {
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   register?: UseFormRegister<any>
   name: string
+  rules?: RegisterOptions
+  error?: FieldError
 }
 
-export function Input({ register, name, ...rest }: InputProps) {
-  return <input {...(register && register(name))} {...rest} />
+export function Input({ register, name, rules, error, ...rest }: InputProps) {
+  return (
+    <Contanier>
+      <div>
+        <input {...(register && register(name, rules))} {...rest} />
+        {error?.message && (
+          <Error title={error.message}>
+            <FiAlertCircle color="#c53030" size={20} />
+          </Error>
+        )}
+        {error?.type === 'required' && (
+          <Error title={`O campo ${name} é obrigatório`}>
+            <FiAlertCircle color="#c53030" size={20} />
+          </Error>
+        )}
+      </div>
+    </Contanier>
+  )
 }
 
 type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
---
+++ src/components/NewForm/styles.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+import Tooltip from '../Tooltip'
+
+export const Contanier = styled.div`
+  border-radius: 10px;
+  width: 100%;
+
+  > label {
+    font-weight: normal;
+  }
+
+  & > div {
+    position: relative;
+  }
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 1px solid #c2cad8;
+    padding-right: 30px;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  width: fit-content;
+  position: absolute;
+  right: 5px;
+  margin: 0;
+  top: 50%;
+  transform: translateY(-50%);
+  svg {
+    margin: 0;
+  }
+
+  span {
+    width: max-content;
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`
