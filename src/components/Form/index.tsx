3284c429c6c96661e2487a7c0d93e08864cc288f
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-07-16 10:15:30 -0300

    refactor: reaname NewForm to Form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,24 +1,141 @@
-import React from 'react'
-import { FormHandles } from '@unform/core'
-import { Form as FormUnForm } from '@unform/web'
+import React, {
+  InputHTMLAttributes,
+  SelectHTMLAttributes,
+  useEffect,
+  ReactElement
+} from 'react'
+import {
+  FieldError,
+  RegisterOptions,
+  useForm,
+  UseFormRegister
+} from 'react-hook-form'
 
-type TypesFormProps<T> = {
-  formRef: React.Ref<FormHandles>
-  onSubmitForm: (data: T) => Promise<void>
-  children?: JSX.Element
-}
+import { IconBaseProps } from 'react-icons'
+import { FiAlertCircle } from 'react-icons/fi'
+import { Contanier, Error, SelectContanier } from './styles'
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-const Form = <T extends {}>({
-  formRef,
-  onSubmitForm,
-  children
-}: TypesFormProps<T>): JSX.Element => {
+export default function Form({ defaultValues, children, onSubmit }: any) {
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors }
+  } = useForm({ defaultValues })
+
+  useEffect(() => {
+    reset(defaultValues)
+  }, [defaultValues, reset])
+
+  function registeredField(child: ReactElement) {
+    return React.createElement(child.type, {
+      ...{
+        ...child.props,
+        register,
+        error: errors[child.props.name],
+        key: child.props.name
+      }
+    })
+  }
+
+  function buildChildren(children: ReactElement, key = 0): any {
+    if (Array.isArray(children)) {
+      return children.map((child: ReactElement, index) => {
+        return buildChildren(child, index)
+      })
+    }
+
+    if (children?.props?.children) {
+      const childCopy = React.cloneElement(children, {
+        key,
+        children: buildChildren(children.props.children)
+      })
+      return childCopy
+    }
+    return children?.props?.name ? registeredField(children) : children
+  }
+
   return (
-    <FormUnForm ref={formRef} onSubmit={onSubmitForm}>
-      {children && children}
-    </FormUnForm>
+    <form onSubmit={handleSubmit(onSubmit)}>{buildChildren(children)}</form>
   )
 }
 
-export default Form
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  register?: UseFormRegister<any>
+  name: string
+  rules?: RegisterOptions
+  error?: FieldError
+  label?: string
+  icon?: React.ComponentType<IconBaseProps>
+}
+
+export function Input({
+  register,
+  name,
+  label,
+  icon: Icon,
+  rules,
+  error,
+  ...rest
+}: InputProps) {
+  return (
+    <Contanier>
+      {Icon && <Icon size={20} />}
+      {label && (
+        <label htmlFor={name} className="control-label">
+          {label}
+        </label>
+      )}
+      <div>
+        <input {...(register && register(name, rules))} {...rest} />
+        {error?.message && (
+          <Error title={error.message}>
+            <FiAlertCircle color="#c53030" size={20} />
+          </Error>
+        )}
+        {error?.type === 'required' && (
+          <Error title={`O campo ${label} é obrigatório`}>
+            <FiAlertCircle color="#c53030" size={20} />
+          </Error>
+        )}
+      </div>
+    </Contanier>
+  )
+}
+
+type SelectProps = SelectHTMLAttributes<HTMLSelectElement> & {
+  register?: UseFormRegister<any>
+  options: Array<{
+    value: string | number
+    name: string | number
+  }>
+  name: string
+  label: string
+}
+
+export function Select({
+  register,
+  options,
+  name,
+  label,
+  ...rest
+}: SelectProps) {
+  return (
+    <SelectContanier>
+      {label && (
+        <label htmlFor={name} className="control-label">
+          {label}
+        </label>
+      )}
+      <div>
+        <select {...(register && register(name))} {...rest}>
+          {options.map(option => (
+            <option key={Math.random()} value={option.value}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </SelectContanier>
+  )
+}
