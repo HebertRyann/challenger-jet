bb155b5226ff1efd438fac2c4596ee67abd02274
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-08-10 09:25:03 -0300

    feat: add details tab

---
+++ src/pages/Warehouse/product/ProductRegister_temp/presentation/components/tabs/details/index.tsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import { TooltipComponent } from '../../../../../../../../components/TooltipComponent'
+import {
+  typeUnitMensuredWeight,
+  typeUnitMensuredDetails
+} from '../../../../domain/data/product/unitMensured'
+import { Container } from './styles'
+import { getError } from '../../../utils/getErrors'
+import { useFormApplication } from '../../../providers/form/FormProvider'
+import {
+  Input,
+  Select,
+  Textarea
+} from '../../../../../../../../components/Form'
+
+export const DetailsTab = (): JSX.Element => {
+  const { errors } = useFormApplication()
+
+  return (
+    <>
+      <Container className="row">
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="Peso medida"
+            message="Selecione a unidade de medida"
+          />
+          <Select
+            name="details.measureWeight"
+            options={typeUnitMensuredWeight.map(({ label, value }) => ({
+              value,
+              name: label
+            }))}
+            controlled
+            blank
+          />
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent label="Peso" message="Informe o peso" />
+          <Input className="form-control" name="details.weight" />
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="Dimensão medida"
+            message="Selecione a unidade de medida"
+          />
+          <Select
+            name="details.unitMensured"
+            options={typeUnitMensuredDetails.map(({ label, value }) => ({
+              value,
+              name: label
+            }))}
+            controlled
+            blank
+          />
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent label="Largura" message="Informe a largura" />
+          <Input className="form-control" name="details.width" />
+        </div>
+      </Container>
+      <Container className="row">
+        <div className="form-content col-md-3">
+          <TooltipComponent label="Altura" message="Informe a altura" />
+          <Input className="form-control" name="details.height" />
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent
+            label="Comprimento"
+            message="Informe o comprimento"
+          />
+          <Input className="form-control" name="details.length" />
+        </div>
+        <div className="form-content col-md-3">
+          <TooltipComponent label="Espessura" message="Informe a espessura" />
+          <Input className="form-control" name="details.thickness" />
+        </div>
+      </Container>
+      <Container className="row">
+        <div className="form-content col-md-12">
+          <TooltipComponent
+            label="Descrição e detalhes"
+            message="Informe a descrição e detalhes"
+          />
+          <Textarea
+            name="details.descriptionAndDetails"
+            className={`${getError(errors?.details?.descriptionAndDetails)}`}
+          />
+        </div>
+      </Container>
+      <Container className="row">
+        <div className="form-content col-md-12">
+          <TooltipComponent
+            label="Especificação Técnica"
+            message="Informe a especificação Técnica"
+          />
+          <Textarea
+            name="details.technicalSpecification"
+            className={`${getError(errors?.details?.technicalSpecification)}`}
+          />
+        </div>
+      </Container>
+      <Container className="row">
+        <div className="form-content col-md-12">
+          <TooltipComponent
+            label="Forma de utilização"
+            message="Informe a forma de utilização"
+          />
+          <Textarea
+            name="details.wayOfUse"
+            className={`${getError(errors?.details?.wayOfUse)}`}
+          />
+        </div>
+      </Container>
+    </>
+  )
+}
---
+++ src/pages/Warehouse/product/ProductRegister_temp/presentation/components/tabs/details/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  margin-top: 20px !important;
+
+  textarea {
+    width: 100%;
+    border: 1px solid #c2cad8;
+    height: 90px;
+    padding: 5px 10px;
+  }
+`
