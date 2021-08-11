278b236ec66c2b5425792a6ea0f4a3c4dca5f204
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-08-11 18:04:45 -0300

    feat: add form routes

--- src/routes/form/FormRoute.tsx
+++ src/routes/form/FormRoute.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import { CreateProductPage } from '../../pages/Warehouse/product/ProductRegister_temp/presentation/pages/create'
+import Route from '../Route'
+import { CreateProductPage } from '../../pages/Warehouse/product/ProductRegister_temp/pages/create'
+import ListProductPage from '../../pages/Warehouse/product/ProductRegister_temp/pages/List'
+import { UpdateProductPage } from '../../pages/Warehouse/product/ProductRegister_temp/pages/update'
+import ViewProductPage from '../../pages/Warehouse/product/ProductRegister_temp/pages/View'
 
 export const Form = (): JSX.Element => (
   <>
-    <Route path="/form" exact component={CreateProductPage} isPrivate />
+    <Route path="/form" exact component={ListProductPage} isPrivate />
+    <Route path="/form/create" component={CreateProductPage} isPrivate />
+    <Route path="/form/update/:id" component={UpdateProductPage} isPrivate />
+    <Route path="/form/view/:id" component={ViewProductPage} isPrivate />
   </>
 )
