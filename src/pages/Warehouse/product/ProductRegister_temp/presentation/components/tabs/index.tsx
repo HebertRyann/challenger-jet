e7cac7b4bc1f2269575b2c75badf2b0510617b52
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-08-11 18:06:32 -0300

    feat: add variation tab

--- src/pages/Warehouse/product/ProductRegister_temp/presentation/components/tabs/index.tsx
+++ src/pages/Warehouse/product/ProductRegister_temp/presentation/components/tabs/index.tsx
@@ -3,8 +3,12 @@ import Button from '../../../../../../../components/Button'
 import Form from '../../../../../../../components/Form'
 import { useTabs } from '../../../../../../../hooks/tabs'
 import { TabsModel } from '../../../domain/models/tabs'
+import { DetailsTab } from './details'
 import { OverviewTab } from './overview'
+import { HasVariationTab } from './HasVariation'
 import { StockTab } from './stock'
+import { PriceCompositionTab } from './PriceComposition'
+
 import {
   Container,
   ContentItem,
@@ -12,21 +16,40 @@ import {
   TabName,
   TabPanelContainer
 } from './styles'
+import api from '../../../../../../../services/api'
+import { listById } from '../../../domain/data/api'
+import { ProductResponse } from '../../../../ProductRegister/Update/domain/productResponse'
 
 type TypeContentProps = {
   tabList: TabsModel[]
+  id?: string
 }
 
-export const Tab = ({ tabList }: TypeContentProps): JSX.Element => {
+export const Tab = ({ tabList, id }: TypeContentProps): JSX.Element => {
   const { addTab, changeCurrentTab, loadCurrentTab, loadTabs } = useTabs()
   const overviewFields = OverviewTab()
+  const detailsFields = DetailsTab()
+  const hasVariationFields = HasVariationTab()
   const stockFields = StockTab()
+  const priceCompositionFields = PriceCompositionTab()
 
   useEffect(() => {
     tabList.forEach(({ name, isDefault, label, isEnable }) => {
       addTab({ default: isDefault, name, label, isEnable })
     })
   }, [tabList, addTab])
+
+  async function getProduct(id: string) {
+    const { data } = await api.get<ProductResponse>(listById(id))
+    console.log('product', data)
+    return data
+  }
+
+  useEffect(() => {
+    if (id) {
+      const data = getProduct(id)
+    }
+  }, [id])
 
   const onClickNameTab = (name: string) => changeCurrentTab(name)
 
@@ -36,7 +59,7 @@ export const Tab = ({ tabList }: TypeContentProps): JSX.Element => {
 
   const [values, setValues] = useState({
     overview: { typeProduct: 'venda' },
-    hasVariation: 'NAO'
+    hasVariation: 'NO'
   })
 
   return (
@@ -63,9 +86,26 @@ export const Tab = ({ tabList }: TypeContentProps): JSX.Element => {
             >
               {overviewFields}
             </div>
-            <div className={`${loadCurrentTab().key !== 'stock' && 'hidden'}`}>
-              {stockFields}
+            <div
+              className={`${loadCurrentTab().key !== 'details' && 'hidden'}`}
+            >
+              {detailsFields}
             </div>
+            <div
+              className={`${
+                loadCurrentTab().key !== 'priceComposition' && 'hidden'
+              }`}
+            >
+              {priceCompositionFields}
+            </div>
+            <div
+              className={`${
+                loadCurrentTab().key !== 'hasVariation' && 'hidden'
+              }`}
+            >
+              {hasVariationFields}
+            </div>
+            {loadCurrentTab().key === 'stock' && stockFields}
             <div className="form-actions right">
               <Button type="submit" className="btn dark btn-sm sbold uppercase">
                 Salvar
